e3dd0b50eadf6332751f6958fa8d7c990267431e
MengxinLiu <mengxin@alauda.io>
2019-01-21 22:33:39 +0800

    refactor: remove link operation to cniserver, simplify plugin logical

--- cmd/cni/cni.go
+++ cmd/cni/cni.go
@@ -10,7 +10,6 @@ import (
 	"github.com/containernetworking/cni/pkg/types/current"
 	"github.com/containernetworking/cni/pkg/version"
 	"github.com/containernetworking/plugins/pkg/ns"
-	"github.com/vishvananda/netlink"
 	"net"
 	"runtime"
 	"strings"
@@ -48,19 +47,6 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 	client := request.NewCniServerClient(n.ServerSocket)
 
-	hostNicName, containerNicName := generateNicName(args.ContainerID)
-	veth := netlink.Veth{LinkAttrs: netlink.LinkAttrs{Name: hostNicName}, PeerName: containerNicName}
-	defer func() {
-		if err != nil {
-			netlink.LinkDel(&veth)
-		}
-	}()
-
-	err = netlink.LinkAdd(&veth)
-	if err != nil {
-		return fmt.Errorf("failed to crate veth for %s %v", podName, err)
-	}
-
 	res, err := client.Add(request.PodRequest{
 		PodName:      podName,
 		PodNamespace: podNamespace,
@@ -71,20 +57,6 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 
 	result := generateCNIResult(cniVersion, res)
-	macAddr, err := net.ParseMAC(res.MacAddress)
-	if err != nil {
-		return fmt.Errorf("failed to parse mac")
-	}
-
-	err = configureHostNic(hostNicName, macAddr)
-	if err != nil {
-		return err
-	}
-
-	err = configureContainerNic(containerNicName, result.IPs[0].Address.String(), macAddr, netns)
-	if err != nil {
-		return err
-	}
 	return types.PrintResult(&result, cniVersion)
 }
 
@@ -97,70 +69,11 @@ func generateCNIResult(cniVersion string, podResponse *request.PodResponse) curr
 		Gateway: net.ParseIP(podResponse.Gateway).To4(),
 	}
 	result.IPs = []*current.IPConfig{&ip}
-
 	route := types.Route{}
 	route.Dst = net.IPNet{IP: net.ParseIP("0.0.0.0").To4(), Mask: net.CIDRMask(0, 32)}
 	route.GW = net.ParseIP(podResponse.Gateway).To4()
 	result.Routes = []*types.Route{&route}
 	return result
-}
-
-func configureHostNic(nicName string, macAddr net.HardwareAddr) error {
-	hostLink, err := netlink.LinkByName(nicName)
-	if err != nil {
-		return fmt.Errorf("can not find host nic %s %v", nicName, err)
-	}
-
-	err = netlink.LinkSetHardwareAddr(hostLink, macAddr)
-	if err != nil {
-		return fmt.Errorf("can not set mac address to host nic %s %v", nicName, err)
-	}
-	err = netlink.LinkSetUp(hostLink)
-	if err != nil {
-		return fmt.Errorf("can not set host nic %s up %v", nicName, err)
-	}
-	return nil
-}
-
-func configureContainerNic(nicName, ipAddr string, macaddr net.HardwareAddr, netns ns.NetNS) error {
-	containerLink, err := netlink.LinkByName(nicName)
-	if err != nil {
-		return fmt.Errorf("can not find container nic %s %v", nicName, err)
-	}
-
-	err = netlink.LinkSetNsFd(containerLink, int(netns.Fd()))
-	if err != nil {
-		return fmt.Errorf("failed to link netns %v", err)
-	}
-
-	return ns.WithNetNSPath(netns.Path(), func(_ ns.NetNS) error {
-		err = netlink.LinkSetName(containerLink, "eth0")
-		if err != nil {
-			return err
-		}
-		addr, err := netlink.ParseAddr(ipAddr)
-		if err != nil {
-			return fmt.Errorf("can not parse %s %v", ipAddr, err)
-		}
-		err = netlink.AddrAdd(containerLink, addr)
-		if err != nil {
-			return fmt.Errorf("can not add address to container nic %v", err)
-		}
-
-		err = netlink.LinkSetHardwareAddr(containerLink, macaddr)
-		if err != nil {
-			return fmt.Errorf("can not set mac address to container nic %v", err)
-		}
-		err = netlink.LinkSetUp(containerLink)
-		if err != nil {
-			return fmt.Errorf("can not set container nic %s up %v", nicName, err)
-		}
-		return nil
-	})
-}
-
-func generateNicName(containerID string) (string, string) {
-	return fmt.Sprintf("%s_h", containerID[0:12]), fmt.Sprintf("%s_c", containerID[0:12])
 }
 
 func cmdDel(args *skel.CmdArgs) error {
@@ -179,23 +92,6 @@ func cmdDel(args *skel.CmdArgs) error {
 		return err
 	}
 
-	hostNicName, _ := generateNicName(args.ContainerID)
-	l, err := netlink.LinkByName(hostNicName)
-	if _, ok := err.(netlink.LinkNotFoundError); ok {
-		return client.Del(request.PodRequest{
-			PodName:      podName,
-			PodNamespace: podNamespace,
-			ContainerID:  args.ContainerID,
-			NetNs:        args.Netns})
-	}
-	if err != nil {
-		return fmt.Errorf("failed to get link %s %v", l.Attrs().Name, err)
-	}
-
-	err = netlink.LinkDel(l)
-	if err != nil {
-		return fmt.Errorf("delete link %s failed %v", l.Attrs().Name, err)
-	}
 	return client.Del(request.PodRequest{
 		PodName:      podName,
 		PodNamespace: podNamespace,
--- pkg/daemon/ovs.go
+++ pkg/daemon/ovs.go
@@ -2,70 +2,139 @@ package daemon
 
 import (
 	"fmt"
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/vishvananda/netlink"
+	"net"
 	"os/exec"
 )
 
 func (csc CniServerHandler) configureNic(podName, podNamespace, netns, containerID, mac, ip string) error {
-	nicName := generateNicName(containerID)
+	var err error
+	hostNicName, containerNicName := generateNicName(containerID)
 
-	// 1. add ovs port
-	output, err := exec.Command("ovs-vsctl", "add-port", "br-int", nicName+"_h", "--", "set", "interface", nicName+"_h", fmt.Sprintf("external_ids:iface-id=%s.%s", podName, podNamespace)).CombinedOutput()
+	// Create a veth pair, put one end to container ,the other to ovs port
+	// NOTE: DO NOT use ovs internal type interface for container.
+	// Kubernetes will detect 'eth0' nic in pod, so the nic name in pod must be 'eth0'.
+	// When renaming internal interface to 'eth0', ovs will delete and recreate this interface.
+	veth := netlink.Veth{LinkAttrs: netlink.LinkAttrs{Name: hostNicName}, PeerName: containerNicName}
+	defer func() {
+		// Remove veth link in case any error during creating pod network.
+		if err != nil {
+			netlink.LinkDel(&veth)
+		}
+	}()
+	err = netlink.LinkAdd(&veth)
+	if err != nil {
+		return fmt.Errorf("failed to crate veth for %s %v", podName, err)
+	}
+
+	// Add veth pair host end to ovs port
+	output, err := exec.Command("ovs-vsctl", "add-port", "br-int", hostNicName, "--", "set", "interface", hostNicName, fmt.Sprintf("external_ids:iface-id=%s.%s", podName, podNamespace)).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("add nic to ovs failed %v: %s", err, output)
 	}
-	//output, err = exec.Command("ovs-vsctl", "set", "Interface", nicName+"_h", fmt.Sprintf("external_ids:iface-id=%s", fmt.Sprintf("%s.%s", podName, podNamespace))).CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("set interface id failed %v: %s", err, output)
-	//}
 
-	//// 2. link netns to /var/run/netns/{container_id}
-	//output, err = exec.Command("ln", "-s", netns, fmt.Sprintf("/var/run/netns/%s", nicName)).CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to link netns %v %s", err, output)
-	//}
-	//
-	//// 3. config nic
-	//output, err = exec.Command("ip", "link", "set", nicName, "netns", nicName).CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to move nic %v %s", err, output)
-	//}
-	//output, err = exec.Command("ip", "netns", "exec", nicName, "ip", "link", "set", nicName, "name", "eth0").CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to rename nic %v %s", err, output)
-	//}
-	//
-	//output, err = exec.Command("ip", "netns", "exec", nicName, "ip", "link", "set", "eth0", "address", mac).CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to set mac %v %s", err, output)
-	//}
-	//output, err = exec.Command("ip", "netns", "exec", nicName, "ip", "addr", "add", ip, "dev", "eth0").CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to set ip %v %s", err, output)
-	//}
-	//output, err = exec.Command("ip", "netns", "exec", nicName, "ip", "link", "set", "dev", "eth0", "up").CombinedOutput()
-	//if err != nil {
-	//	return fmt.Errorf("failed to set ip %v %s", err, output)
-	//}
+	// host and container nic must use same mac address, otherwise ovn will reject these packets by default
+	macAddr, err := net.ParseMAC(mac)
+	if err != nil {
+		return fmt.Errorf("failed to parse mac %s %v", macAddr, err)
+	}
+
+	err = configureHostNic(hostNicName, macAddr)
+	if err != nil {
+		return err
+	}
+
+	podNS, err := ns.GetNS(netns)
+	if err != nil {
+		return fmt.Errorf("failed to open netns %q: %v", netns, err)
+	}
+	err = configureContainerNic(containerNicName, ip, macAddr, podNS)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (csc CniServerHandler) deleteNic(netns, containerID string) error {
-	nicName := generateNicName(containerID)
-
-	output, err := exec.Command("ovs-vsctl", "--if-exists", "--with-iface", "del-port", "br-int", nicName+"_h").CombinedOutput()
+	hostNicName, _ := generateNicName(containerID)
+	// Remove ovs port
+	output, err := exec.Command("ovs-vsctl", "--if-exists", "--with-iface", "del-port", "br-int", hostNicName).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to delete ovs port %v, %s", err, output)
 	}
-	//// TODO: check not exists
-	//if _, err := os.Stat("/var/run/netns/"+nicName); err != nil {
-	//	klog.Infof("stat netns %s failed %v", "/var/run/netns/"+nicName, err)
-	//	return nil
-	//}
-	//os.Remove("/var/run/netns/"+nicName)
+
+	hostLink, err := netlink.LinkByName(hostNicName)
+	if err != nil {
+		// If link already not exists, return quietly
+		if _, ok := err.(netlink.LinkNotFoundError); ok {
+			return nil
+		}
+		return fmt.Errorf("find host link %s failed %v", hostNicName, err)
+	}
+	err = netlink.LinkDel(hostLink)
+	if err != nil {
+		return fmt.Errorf("delete host link %s failed %v", hostLink, err)
+	}
 	return nil
 }
 
-func generateNicName(containerID string) string {
-	return containerID[0:12]
+func generateNicName(containerID string) (string, string) {
+	return fmt.Sprintf("%s_h", containerID[0:12]), fmt.Sprintf("%s_c", containerID[0:12])
+}
+
+func configureHostNic(nicName string, macAddr net.HardwareAddr) error {
+	hostLink, err := netlink.LinkByName(nicName)
+	if err != nil {
+		return fmt.Errorf("can not find host nic %s %v", nicName, err)
+	}
+
+	err = netlink.LinkSetHardwareAddr(hostLink, macAddr)
+	if err != nil {
+		return fmt.Errorf("can not set mac address to host nic %s %v", nicName, err)
+	}
+	err = netlink.LinkSetUp(hostLink)
+	if err != nil {
+		return fmt.Errorf("can not set host nic %s up %v", nicName, err)
+	}
+	return nil
+}
+
+func configureContainerNic(nicName, ipAddr string, macaddr net.HardwareAddr, netns ns.NetNS) error {
+	containerLink, err := netlink.LinkByName(nicName)
+	if err != nil {
+		return fmt.Errorf("can not find container nic %s %v", nicName, err)
+	}
+
+	err = netlink.LinkSetNsFd(containerLink, int(netns.Fd()))
+	if err != nil {
+		return fmt.Errorf("failed to link netns %v", err)
+	}
+
+	return ns.WithNetNSPath(netns.Path(), func(_ ns.NetNS) error {
+		// Container nic name MUST be 'eth0', otherwise kubelet will recreate the pod
+		err = netlink.LinkSetName(containerLink, "eth0")
+		if err != nil {
+			return err
+		}
+		addr, err := netlink.ParseAddr(ipAddr)
+		if err != nil {
+			return fmt.Errorf("can not parse %s %v", ipAddr, err)
+		}
+		err = netlink.AddrAdd(containerLink, addr)
+		if err != nil {
+			return fmt.Errorf("can not add address to container nic %v", err)
+		}
+
+		err = netlink.LinkSetHardwareAddr(containerLink, macaddr)
+		if err != nil {
+			return fmt.Errorf("can not set mac address to container nic %v", err)
+		}
+		err = netlink.LinkSetUp(containerLink)
+		if err != nil {
+			return fmt.Errorf("can not set container nic %s up %v", nicName, err)
+		}
+		return nil
+	})
 }
