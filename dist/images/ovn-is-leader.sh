0d86393d298725de08b28401cbf094811eee0e93
MengxinLiu <mengxin@alauda.io>
2019-05-13 18:40:20 +0800

    fix: use ovn/ovs-ctl to health check

--- dist/images/ovn-is-leader.sh
+++ dist/images/ovn-is-leader.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-ovn-nbctl show
-# wait 5 seconds
-ovn-sbctl -t 5 show
+alias ovn-ctl='/usr/share/openvswitch/scripts/ovn-ctl'
+
+ovn-ctl status_northd
+ovn-ctl status_ovnnb
+ovn-ctl status_ovnsb
 
 # For data consistency, only store leader address in endpoint
 echo $(cat /var/log/openvswitch/ovn-northd.log) | grep -oP "lock acquired(?!.*lock lost).*$"
