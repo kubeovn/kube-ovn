89491b5702ac3dbffca10c7b7e38e0fe2165c48a
MengxinLiu <mengxin@alauda.io>
2019-05-14 13:57:47 +0800

    fix: wait node-gw info ready

--- pkg/daemon/init.go
+++ pkg/daemon/init.go
@@ -1,26 +1,32 @@
 package daemon
 
 import (
-	"fmt"
 	"github.com/alauda/kube-ovn/pkg/util"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
+	"time"
 )
 
 // InitNodeGateway init ovn0
 func InitNodeGateway(config *Configuration) error {
-	nodeName := config.NodeName
-	node, err := config.KubeClient.CoreV1().Nodes().Get(nodeName, v1.GetOptions{})
-	if err != nil {
-		klog.Errorf("failed to get node %s info %v", nodeName, err)
-		return err
-	}
-	macAddr := node.Annotations[util.MacAddressAnnotation]
-	ipAddr := node.Annotations[util.IpAddressAnnotation]
-	portName := node.Annotations[util.PortNameAnnotation]
-	gw := node.Annotations[util.GatewayAnnotation]
-	if macAddr == "" || ipAddr == "" || portName == "" || gw == "" {
-		return fmt.Errorf("can not find macAddr, ipAddr, portName and gw")
+	var portName, ipAddr, macAddr, gw string
+	for {
+		nodeName := config.NodeName
+		node, err := config.KubeClient.CoreV1().Nodes().Get(nodeName, v1.GetOptions{})
+		if err != nil {
+			klog.Errorf("failed to get node %s info %v", nodeName, err)
+			return err
+		}
+		macAddr = node.Annotations[util.MacAddressAnnotation]
+		ipAddr = node.Annotations[util.IpAddressAnnotation]
+		portName = node.Annotations[util.PortNameAnnotation]
+		gw = node.Annotations[util.GatewayAnnotation]
+		if macAddr == "" || ipAddr == "" || portName == "" || gw == "" {
+			klog.Errorf("can not find macAddr, ipAddr, portName and gw, wait 3 seconds...")
+			time.Sleep(3 * time.Second)
+		} else {
+			break
+		}
 	}
 	return configureNodeNic(portName, ipAddr, macAddr, gw)
 }
