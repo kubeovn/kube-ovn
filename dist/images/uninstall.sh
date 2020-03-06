479437a35667e228ba605d1c8800867b1294a377
MengxinLiu <mengxin@alauda.io>
2020-03-06 14:35:09 +0800

    fix: cleanup in offline environment

---
+++ dist/images/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/usr/share/openvswitch/scripts/ovs-ctl stop
+ovs-dpctl del-dp ovs-system
+
+rm -rf /var/run/openvswitch/*
+rm -rf /var/run/ovn/*
+rm -rf /etc/openvswitch/*
+rm -rf /etc/ovn/*
+rm -rf /var/log/openvswitch/*
+rm -rf /var/log/ovn/*
+rm -rf /etc/cni/net.d/00-kube-ovn.conflist
