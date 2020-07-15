31723f669e54f662d9d56c9833c7386a05fe768f
Mengxin Liu <mengxin@alauda.io>
2020-07-15 16:41:13 +0800

    chore: check ovn-sb connectivity from ovn-ovs probe

--- dist/images/ovs-healthcheck.sh
+++ dist/images/ovs-healthcheck.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 shopt -s expand_aliases
 
+echo Connecting OVN SB "${OVN_SB_SERVICE_HOST}":"${OVN_SB_SERVICE_PORT}"
+ovn-sbctl --db=tcp:["${OVN_SB_SERVICE_HOST}"]:"${OVN_SB_SERVICE_PORT}" --timeout=3 show
+
 alias ovs-ctl='/usr/share/openvswitch/scripts/ovs-ctl'
 alias ovn-ctl='/usr/share/ovn/scripts/ovn-ctl'
 
