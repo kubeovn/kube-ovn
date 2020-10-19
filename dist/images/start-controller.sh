7f5b423742d743ff4a9213df19c685f49c3532f6
Mengxin Liu <mengxin@alauda.io>
2020-10-19 09:49:06 +0800

    fix: add default ssl var for compatibility

--- dist/images/start-controller.sh
+++ dist/images/start-controller.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+ENABLE_SSL=${ENABLE_SSL:-false}
 
 if [[ "$ENABLE_SSL" == "false" ]]; then
   export OVN_NB_DAEMON=$(ovn-nbctl --db=tcp:["${OVN_NB_SERVICE_HOST}"]:"${OVN_NB_SERVICE_PORT}" --pidfile --detach --overwrite-pidfile)
