37cbb713ed95d3b03b30ebbf75f2c34d8a5067f6
hzma <hzma@alauda.io>
2020-11-23 10:16:31 +0800

    add node ping total count metric

--- pkg/pinger/ping.go
+++ pkg/pinger/ping.go
@@ -74,7 +74,8 @@ func pingNodes(config *Configuration) {
 						config.PodName,
 						no.Name, addr.Address,
 						float64(stats.AvgRtt)/float64(time.Millisecond),
-						int(math.Abs(float64(stats.PacketsSent-stats.PacketsRecv))))
+						int(math.Abs(float64(stats.PacketsSent-stats.PacketsRecv))),
+						int(float64(stats.PacketsSent)))
 				}(addr.Address, no.Name)
 			}
 		}
@@ -119,7 +120,8 @@ func pingPods(config *Configuration) {
 					nodeIP,
 					podIp,
 					float64(stats.AvgRtt)/float64(time.Millisecond),
-					int(math.Abs(float64(stats.PacketsSent-stats.PacketsRecv))))
+					int(math.Abs(float64(stats.PacketsSent-stats.PacketsRecv))),
+					int(float64(stats.PacketsSent)))
 			}(pod.Status.PodIP, pod.Name, pod.Status.HostIP, pod.Spec.NodeName)
 		}
 	}
