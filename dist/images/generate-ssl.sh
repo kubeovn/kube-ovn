6483d6e330cb4f46a9e260d0d65add82c757ab91
hzma <hzma@alauda.io>
2020-12-09 13:37:24 +0800

    modify build problem

--- dist/images/generate-ssl.sh
+++ dist/images/generate-ssl.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ovs-pki init --force
+ovs-pki init -l /dev/stdout --force
 cp /var/lib/openvswitch/pki/switchca/cacert.pem /etc/ovn/
 cd /etc/ovn
-ovs-pki req ovn --force
-ovs-pki -b sign ovn --force
+ovs-pki req ovn -l /dev/stdout --force
+ovs-pki -b sign ovn -l /dev/stdout --force
