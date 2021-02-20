c7733499e7e915bcc6c202c7359f2a4b9ae9ea07
Yan Wei <yanw@t3go.cn>
2021-02-20 22:58:38 +0800

    Update install-pre-1.16.sh
    Optimize Command

--- dist/images/install-pre-1.16.sh
+++ dist/images/install-pre-1.16.sh
@@ -2107,7 +2107,7 @@ checkKubeProxy(){
     nodeIps=`kubectl get node -o wide --no-headers | awk '{print $6}'`
     for node in $nodeIps
     do
-      healthResult=`curl -g -6 -sL -w %{http_code} http://[$node]:10256/healthz -o /dev/null | grep -v 200 || true`
+      healthResult=`curl -g -6 -sL --connect-timeout 5 -w %{http_code} http://[$node]:10256/healthz -o /dev/null | grep -v 200 || true`
       if [ -n "$healthResult" ]; then
         echo "$node kube-proxy's health check failed"
         exit 1
