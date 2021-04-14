9e82ca3d594ce169dc575f0eddf8f09c1918c162
Mengxin Liu <mengxin@alauda.io>
2021-04-14 09:28:17 +0800

    fix: release norhtd lock when power off
    When the node that runs northd power off,
    the lock to southbound will not released.
    Use ep to store the norhtd leader and try
    to release lock if no alive northd.

--- dist/images/install-pre-1.16.sh
+++ dist/images/install-pre-1.16.sh
@@ -693,6 +693,24 @@ spec:
 kind: Service
 apiVersion: v1
 metadata:
+  name: ovn-northd
+  namespace: kube-system
+spec:
+  ports:
+    - name: ovn-northd
+      protocol: TCP
+      port: 6643
+      targetPort: 6643
+  type: ClusterIP
+  selector:
+    app: ovn-central
+    ovn-northd-leader: "true"
+  sessionAffinity: None
+
+---
+kind: Service
+apiVersion: v1
+metadata:
   name: kube-ovn-monitor
   namespace: kube-system
   labels:
@@ -1185,6 +1203,23 @@ spec:
   selector:
     app: ovn-central
     ovn-sb-leader: "true"
+  sessionAffinity: None
+---
+kind: Service
+apiVersion: v1
+metadata:
+  name: ovn-northd
+  namespace: kube-system
+spec:
+  ports:
+    - name: ovn-northd
+      protocol: TCP
+      port: 6643
+      targetPort: 6643
+  type: ClusterIP
+  selector:
+    app: ovn-central
+    ovn-northd-leader: "true"
   sessionAffinity: None
 ---
 kind: Service
