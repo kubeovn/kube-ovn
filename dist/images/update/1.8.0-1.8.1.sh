348eaf3670720181c1e5a82ca10a1bc02a3c1732
Mengxin Liu <mengxin@alauda.io>
2021-10-27 14:39:37 +0800

    update: add 1.7 to 1.8 update scripts

--- dist/images/update/1.8.0-1.8.1.sh
+++ dist/images/update/1.8.0-1.8.1.sh
@@ -6,26 +6,41 @@ IMAGE=kubeovn/kube-ovn:v1.8.1
 echo "[Step 0/5] Update ovn-central"
 kubectl set image deployment/ovn-central -n kube-system ovn-central="$IMAGE"
 kubectl rollout status deployment/ovn-central -n kube-system
+echo "-------------------------------"
+echo ""
 
 echo "[Step 1/5] Update ovs-ovn"
 kubectl set image ds/ovs-ovn -n kube-system openvswitch="$IMAGE"
+kubectl delete pod -n kube-system -lapp=ovs
+echo "-------------------------------"
+echo ""
 
 echo "[Step 2/5] Update kube-ovn-controller"
 kubectl set image deployment/kube-ovn-controller -n kube-system kube-ovn-controller="$IMAGE"
 kubectl rollout status deployment/kube-ovn-controller -n kube-system
+echo "-------------------------------"
+echo ""
 
 echo "[Step 3/5] Update kube-ovn-cni"
 kubectl set image ds/kube-ovn-cni -n kube-system cni-server="$IMAGE"
 kubectl rollout status daemonset/kube-ovn-cni -n kube-system
+echo "-------------------------------"
+echo ""
 
 echo "[Step 4/5] Update kube-ovn-pinger"
-kubectl patch ds/kube-ovn-pinger -n kube-system --type='json' -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
+if [[ $(kubectl get ds -n kube-system kube-ovn-pinger -o jsonpath='{.spec.template}') =~ "tolerations" ]]; then
+  kubectl patch ds/kube-ovn-pinger -n kube-system --type='json' -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
+fi
 kubectl set image ds/kube-ovn-pinger -n kube-system pinger="$IMAGE"
 kubectl rollout status daemonset/kube-ovn-pinger -n kube-system
+echo "-------------------------------"
+echo ""
 
 echo "[Step 5/5] Update kube-ovn-monitor"
 kubectl set image deployment/kube-ovn-monitor -n kube-system kube-ovn-monitor="$IMAGE"
 kubectl rollout status deployment/kube-ovn-monitor -n kube-system
+echo "-------------------------------"
+echo ""
 
 echo "Update Success!"
 echo "
