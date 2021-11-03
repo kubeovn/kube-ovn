7adf4ea776192d63783110718bada2e15c11fdec
范日明 <fanriming@chinatelecom.cn>
2021-11-03 15:06:25 +0800

    do not set bridge-nf-call-iptables

--- dist/images/install-cni.sh
+++ dist/images/install-cni.sh
@@ -2,10 +2,6 @@
 
 set -u -e
 
-if [[ -f "/proc/sys/net/bridge/bridge-nf-call-iptables" ]];
-    then echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables;
-fi
-
 if [[ -f "/proc/sys/net/ipv4/ip_forward" ]];
     then echo 1 > /proc/sys/net/ipv4/ip_forward;
 fi
