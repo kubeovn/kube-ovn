5ab83ba42b41e4dbab912be345a42adc0fdefdd1
zhangzujian <zhangzujian.7@gmail.com>
2021-12-30 15:31:25 +0800

    fix installation script
    1. add timeout for kubectl rollout status;
    2. sleep 5 seconds after deleting Pods.

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -1825,7 +1825,7 @@ fi
 
 kubectl apply -f kube-ovn-crd.yaml
 kubectl apply -f ovn.yaml
-kubectl rollout status deployment/ovn-central -n kube-system
+kubectl rollout status deployment/ovn-central -n kube-system --timeout 300s
 echo "-------------------------------"
 echo ""
 
@@ -2413,8 +2413,8 @@ spec:
 EOF
 
 kubectl apply -f kube-ovn.yaml
-kubectl rollout status deployment/kube-ovn-controller -n kube-system
-kubectl rollout status daemonset/kube-ovn-cni -n kube-system
+kubectl rollout status deployment/kube-ovn-controller -n kube-system --timeout 300s
+kubectl rollout status daemonset/kube-ovn-cni -n kube-system --timeout 300s
 echo "-------------------------------"
 echo ""
 
@@ -2425,8 +2425,9 @@ for ns in $(kubectl get ns --no-headers -o  custom-columns=NAME:.metadata.name);
   done
 done
 
-kubectl rollout status daemonset/kube-ovn-pinger -n kube-system
-kubectl rollout status deployment/coredns -n kube-system
+sleep 5
+kubectl rollout status daemonset/kube-ovn-pinger -n kube-system --timeout 300s
+kubectl rollout status deployment/coredns -n kube-system --timeout 300s
 echo "-------------------------------"
 echo ""
 
@@ -2951,7 +2952,6 @@ fi
 
 echo "[Step 6/6] Run network diagnose"
 kubectl ko diagnose all
-
 
 echo "-------------------------------"
 echo "
