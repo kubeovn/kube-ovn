f210b93403240a13cbe8d2a704ba0338d088dd79
hzma <hzma@alauda.io>
2022-01-14 16:36:18 +0800

    add back centralized subnet active-standby mode

--- pkg/controller/node.go
+++ pkg/controller/node.go
@@ -578,6 +578,9 @@ func (c *Controller) CheckGatewayReady() {
 }
 
 func (c *Controller) checkGatewayReady() error {
+	if !c.config.EnableEcmp {
+		return nil
+	}
 	klog.V(3).Infoln("start to check gateway status")
 	subnetList, err := c.subnetsLister.List(labels.Everything())
 	if err != nil {
