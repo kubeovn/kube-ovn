4022bd577cbe142a264c8f7544332711c271d95f
hzma <hzma@alauda.io>
2022-01-19 14:01:19 +0800

    add metric for ovn nb/sb db status

--- pkg/ovnmonitor/exporter.go
+++ pkg/ovnmonitor/exporter.go
@@ -144,6 +144,7 @@ func (e *Exporter) ovnMetricsUpdate() {
 		e.exportOvnLogFileSizeGauge()
 		e.exportOvnDBFileSizeGauge()
 		e.exportOvnRequestErrorGauge()
+		e.exportOvnDBStatusGauge()
 
 		e.exportOvnChassisGauge()
 		e.exportLogicalSwitchGauge()
@@ -251,5 +252,21 @@ func (e *Exporter) exportOvnClusterInfoGauge() {
 			return
 		}
 		e.setOvnClusterInfoMetric(clusterStatus, database)
+	}
+}
+
+func (e *Exporter) exportOvnDBStatusGauge() {
+	dbList := []string{"OVN_Northbound", "OVN_Southbound"}
+	for _, database := range dbList {
+		ok, err := getDBStatus(database)
+		if err != nil {
+			klog.Errorf("Failed to get DB status for %s: %v", database, err)
+			return
+		}
+		if ok {
+			metricDBStatus.WithLabelValues(e.Client.System.Hostname, database).Set(1)
+		} else {
+			metricDBStatus.WithLabelValues(e.Client.System.Hostname, database).Set(0)
+		}
 	}
 }
