da6e04e373199985821e19ec43d406357a7f460c
xujunjie-cover <xujunjielxx@163.com>
2022-02-25 15:29:29 +0800

    Update gateway.go

--- pkg/daemon/gateway.go
+++ pkg/daemon/gateway.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/alauda/felix/ipsets"
 	"github.com/vishvananda/netlink"
-        k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
