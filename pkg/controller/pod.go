553bedd2fab1147f1037f71399b08a093873af5a
zhangzujian <zhangzujian.7@gmail.com>
2022-04-07 17:19:42 +0800

    fix adding key to delete Pod queue

--- pkg/controller/pod.go
+++ pkg/controller/pod.go
@@ -554,7 +554,7 @@ func (c *Controller) handleAddPod(key string) error {
 		if k8serrors.IsNotFound(err) {
 			// Sometimes pod is deleted between kube-ovn configure ovn-nb and patch pod.
 			// Then we need to recycle the resource again.
-			c.deletePodQueue.AddRateLimited(key)
+			c.deletePodQueue.AddRateLimited(pod)
 			return nil
 		}
 		klog.Errorf("patch pod %s/%s failed: %v", name, namespace, err)
@@ -846,7 +846,7 @@ func (c *Controller) handleUpdatePod(key string) error {
 		if k8serrors.IsNotFound(err) {
 			// Sometimes pod is deleted between kube-ovn configure ovn-nb and patch pod.
 			// Then we need to recycle the resource again.
-			c.deletePodQueue.AddRateLimited(key)
+			c.deletePodQueue.AddRateLimited(pod)
 			return nil
 		}
 		klog.Errorf("patch pod %s/%s failed %v", name, namespace, err)
