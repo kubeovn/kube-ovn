4707883226b0d9af5e3939ec97ef97b52492fbff
zhangzujian <zhangzujian.7@gmail.com>
2022-04-07 14:28:38 +0800

    fix IPAM initialization

--- pkg/controller/init.go
+++ pkg/controller/init.go
@@ -335,7 +335,7 @@ func (c *Controller) InitIPAM() error {
 				if err != nil {
 					klog.Errorf("failed to init pod %s.%s address %s: %v", podName, pod.Namespace, pod.Annotations[fmt.Sprintf(util.IpAddressAnnotationTemplate, podNet.ProviderName)], err)
 				} else {
-					err = c.createOrUpdateCrdIPs(key, ip, mac, subnet, pod.Namespace, pod.Spec.NodeName, podNet.ProviderName)
+					err = c.createOrUpdateCrdIPs(podName, ip, mac, subnet, pod.Namespace, pod.Spec.NodeName, podNet.ProviderName)
 					if err != nil {
 						klog.Errorf("failed to create/update ips CR %s.%s with ip address %s: %v", podName, pod.Namespace, ip, err)
 					}
