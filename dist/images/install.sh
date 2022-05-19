5f23adc6dd7f1d8c9f5205527d48a5b010c59085
张祖建 <zhangzujian.7@gmail.com>
2022-05-19 21:21:02 +0800

    use leases for leader election (#1529)

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -884,14 +884,6 @@ EOF
 
 if $DPDK; then
   cat <<EOF > ovn.yaml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: ovn-config
-  namespace: kube-system
-data:
-  defaultNetworkType: '$NETWORK_TYPE'
-
 ---
 apiVersion: v1
 kind: ServiceAccount
@@ -984,6 +976,12 @@ rules:
       - create
       - patch
       - update
+  - apiGroups:
+      - coordination.k8s.io
+    resources:
+      - leases
+    verbs:
+      - "*"
   - apiGroups:
       - "k8s.cni.cncf.io"
     resources:
@@ -1377,13 +1375,6 @@ EOF
 
 else
   cat <<EOF > ovn.yaml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: ovn-config
-  namespace: kube-system
-data:
-  defaultNetworkType: '$NETWORK_TYPE'
 ---
 apiVersion: v1
 kind: ServiceAccount
@@ -1465,6 +1456,12 @@ rules:
       - create
       - patch
       - update
+  - apiGroups:
+      - coordination.k8s.io
+    resources:
+      - leases
+    verbs:
+      - "*"
   - apiGroups:
       - "k8s.cni.cncf.io"
     resources:
