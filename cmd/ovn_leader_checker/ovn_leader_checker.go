55b3d508392276c5104500ee52f7537ea8111548
zhangzujian <zhangzujian.7@gmail.com>
2022-07-15 08:31:06 +0800

    ignore ovsdb-server/compact error: not storing a duplicate snapshot

--- cmd/ovn_leader_checker/ovn_leader_checker.go
+++ cmd/ovn_leader_checker/ovn_leader_checker.go
@@ -1,18 +1,18 @@
 package ovn_leader_checker
 
 import (
-	"github.com/kubeovn/kube-ovn/pkg/ovn_leader_checker"
 	"k8s.io/klog/v2"
+
+	"github.com/kubeovn/kube-ovn/pkg/ovn_leader_checker"
 )
 
 func CmdMain() {
 	cfg, err := ovn_leader_checker.ParseFlags()
 	if err != nil {
-		klog.Errorf("ovn_leader_checker parseFlags error %v", err)
+		klog.Fatalf("ovn_leader_checker parseFlags error %v", err)
 	}
-	err = ovn_leader_checker.KubeClientInit(cfg)
-	if err != nil {
-		klog.Errorf("KubeClientInit err %v", err)
+	if err = ovn_leader_checker.KubeClientInit(cfg); err != nil {
+		klog.Fatalf("KubeClientInit err %v", err)
 	}
 	ovn_leader_checker.StartOvnLeaderCheck(cfg)
 }
