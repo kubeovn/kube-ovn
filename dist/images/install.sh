4dad754f210c98ae8661cce0653f5b4a9ee84e0d
zhangzujian <zhangzujian.7@gmail.com>
2022-07-27 10:55:36 +0800

    add e2e testing

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -19,6 +19,9 @@ ENABLE_EXTERNAL_VPC=${ENABLE_EXTERNAL_VPC:-true}
 CNI_CONFIG_PRIORITY=${CNI_CONFIG_PRIORITY:-01}
 ENABLE_LB_SVC=${ENABLE_LB_SVC:-false}
 ENABLE_KEEP_VM_IP=${ENABLE_KEEP_VM_IP:-true}
+# exchange link names of OVS bridge and the provider nic
+# in the default provider-network
+EXCHANGE_LINK_NAME=${EXCHANGE_LINK_NAME:-false}
 # The nic to support container network can be a nic name or a group of regex
 # separated by comma, if empty will use the nic that the default route use
 IFACE=${IFACE:-}
@@ -2797,6 +2800,7 @@ spec:
           - --service-cluster-ip-range=$SVC_CIDR
           - --network-type=$NETWORK_TYPE
           - --default-interface-name=$VLAN_INTERFACE_NAME
+          - --default-exchange-link-name=$EXCHANGE_LINK_NAME
           - --default-vlan-id=$VLAN_ID
           - --ls-dnat-mod-dl-dst=$LS_DNAT_MOD_DL_DST
           - --pod-nic-type=$POD_NIC_TYPE
