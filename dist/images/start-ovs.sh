14beb4840878b99c7ab60a6e6dbc739bf1f81545
oilbeater <liumengxinfly@gmail.com>
2022-08-09 10:40:11 +0800

    perf: disable mlockall to reduce memory usage
    mlockall will lock all virtual memory page into physic memory and prevent memory recycle.
    With this option rss size reduced from 39M to 7M

--- dist/images/start-ovs.sh
+++ dist/images/start-ovs.sh
@@ -64,7 +64,7 @@ else
 fi
 
 # Start vswitchd. restart will automatically set/unset flow-restore-wait which is not what we want
-/usr/share/openvswitch/scripts/ovs-ctl start --no-ovsdb-server --system-id=random
+/usr/share/openvswitch/scripts/ovs-ctl start --no-ovsdb-server --system-id=random --no-mlockall
 /usr/share/openvswitch/scripts/ovs-ctl --protocol=udp --dport=6081 enable-protocol
 
 sleep 1
