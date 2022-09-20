8f4ba96dd9f0e8035f14779be029c90d81e465f1
shane965 <xwang4396@gmail.com>
2022-09-20 22:52:08 +0800

    fix: fip unbind can't take effect immediately when conntrack record exists

--- dist/images/vpcnatgateway/nat-gateway.sh
+++ dist/images/vpcnatgateway/nat-gateway.sh
@@ -159,6 +159,7 @@ function del_floating_ip() {
         if [ "$?" -eq 0 ];then
             exec_cmd "iptables -t nat -D EXCLUSIVE_DNAT -d $eip -j DNAT --to-destination $internalIp"
             exec_cmd "iptables -t nat -D EXCLUSIVE_SNAT -s $internalIp -j SNAT --to-source $eip"
+            exec_cmd "conntrack -D -d $eip"
         fi
     done
 }
