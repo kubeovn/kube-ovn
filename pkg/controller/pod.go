f56bb0b015cb709f1c038b1813dbc8f4bfffbf93
hzma <hzma@alauda.io>
2022-10-18 16:16:19 +0800

    add vm pod to ipam by ip when initIPAM (#1974)

--- pkg/controller/pod.go
+++ pkg/controller/pod.go
@@ -1613,5 +1613,9 @@ func getPodType(pod *v1.Pod) string {
 	if ok, _ := isStatefulSetPod(pod); ok {
 		return "StatefulSet"
 	}
+
+	if isVmPod, _ := isVmPod(pod); isVmPod {
+		return util.Vm
+	}
 	return ""
 }
