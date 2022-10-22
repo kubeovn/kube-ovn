264beb59232997594a109403ce33d4bb1d4d45af
张祖建 <zhangzujian.7@gmail.com>
2022-10-22 10:27:28 +0800

    ovs: fix waiting flows in underlay networking (#1983)

--- dist/images/start-ovs.sh
+++ dist/images/start-ovs.sh
@@ -61,11 +61,6 @@ if [[ `nproc` -gt 12 ]]; then
     ovs-vsctl --no-wait set Open_vSwitch . other_config:n-handler-threads=10
 fi
 
-# When ovs-vswitchd starts with this value set as true, it will neither flush or
-# expire previously set datapath flows nor will it send and receive any
-# packets to or from the datapath. Please check ovs-vswitchd.conf.db.5.txt
-ovs-vsctl --no-wait set open_vswitch . other_config:flow-restore-wait="true"
-
 if [ "$HW_OFFLOAD" = "true" ]; then
   ovs-vsctl --no-wait set open_vswitch . other_config:hw-offload=true
 else
@@ -76,6 +71,49 @@ fi
 ovsdb_server_ctl="/var/run/openvswitch/ovsdb-server.$(cat /var/run/openvswitch/ovsdb-server.pid).ctl"
 ovs-appctl -t "$ovsdb_server_ctl" vlog/set jsonrpc:file:err
 ovs-appctl -t "$ovsdb_server_ctl" vlog/set reconnect:file:err
+
+function wait_flows_pre_check() {
+  local devices=""
+  local ips=($(echo $OVN_DB_IPS | sed 's/,/ /g'))
+  for ip in ${ips[*]}; do
+    devices="$devices $(ip route get $ip | grep -oE 'dev [^\s]+' | awk '{print $2}')"
+  done
+
+  bridges=($(ovs-vsctl --no-heading --columns=name find bridge external-ids:vendor=kube-ovn))
+  for br in ${bridges[@]}; do
+    ports=($(ovs-vsctl list-ports $br))
+    for port in ${ports[@]}; do
+      if ! echo $devices | grep -qw "$port"; then
+        continue
+      fi
+
+      port_type=$(ovs-vsctl --no-heading --columns=type find interface name=$port)
+      if [ ! "x$port_type" = 'x""' ]; then
+        continue
+      fi
+
+      if ! ip link show $port | grep -qw "master ovs-system"; then
+        return 1
+      fi
+    done
+  done
+
+  return 0
+}
+
+skip_wait_flows=0
+if ! wait_flows_pre_check; then
+  skip_wait_flows=1
+fi
+
+if [ $skip_wait_flows -eq 0 ]; then
+  # When ovs-vswitchd starts with this value set as true, it will neither flush or
+  # expire previously set datapath flows nor will it send and receive any
+  # packets to or from the datapath. Please check ovs-vswitchd.conf.db.5.txt
+  ovs-vsctl --no-wait set open_vswitch . other_config:flow-restore-wait="true"
+else
+  ovs-vsctl --no-wait set open_vswitch . other_config:flow-restore-wait="false"
+fi
 
 # Start vswitchd. restart will automatically set/unset flow-restore-wait which is not what we want
 /usr/share/openvswitch/scripts/ovs-ctl start --no-ovsdb-server --system-id=random --no-mlockall
@@ -182,19 +220,21 @@ else
   /usr/share/ovn/scripts/ovn-ctl --ovn-controller-ssl-key=/var/run/tls/key --ovn-controller-ssl-cert=/var/run/tls/cert --ovn-controller-ssl-ca-cert=/var/run/tls/cacert restart_controller
 fi
 
-# Wait ovn-controller finish init flow compute and update it to vswitchd,
-# then update flow-restore-wait to indicate vswitchd to process flows
-set +e
-flow_num=$(ovs-ofctl dump-flows br-int | wc -l)
-while [ $flow_num -le $FLOW_LIMIT ]
-do
-  echo "$flow_num flows now, waiting for ovs-vswitchd flow ready"
-  sleep 1
+if [ $skip_wait_flows -eq 0 ]; then
+  # Wait ovn-controller finish init flow compute and update it to vswitchd,
+  # then update flow-restore-wait to indicate vswitchd to process flows
+  set +e
   flow_num=$(ovs-ofctl dump-flows br-int | wc -l)
-done
-set -e
+  while [ $flow_num -le $FLOW_LIMIT ]
+  do
+    echo "$flow_num flows now, waiting for ovs-vswitchd flow ready"
+    sleep 1
+    flow_num=$(ovs-ofctl dump-flows br-int | wc -l)
+  done
+  set -e
 
-ovs-vsctl --no-wait set open_vswitch . other_config:flow-restore-wait="false"
+  ovs-vsctl --no-wait set open_vswitch . other_config:flow-restore-wait="false"
+fi
 
 set +e
 for netns in /var/run/netns/*; do
