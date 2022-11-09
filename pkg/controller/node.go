2cab58da16ec72ae952730171ddae31327580eb2
lut777 <lut777@users.noreply.github.com>
2022-11-10 00:15:13 +0800

    fix: gateway route should stay still when node is pingable (#2011)

--- pkg/controller/node.go
+++ pkg/controller/node.go
@@ -764,6 +764,7 @@ func (c *Controller) checkGatewayReady() error {
 						pinger.Interval = 1 * time.Second
 
 						success := false
+
 						pinger.OnRecv = func(p *goping.Packet) {
 							success = true
 							pinger.Stop()
