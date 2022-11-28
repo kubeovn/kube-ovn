3f57e1e232e1e41c776bcbe659103ea0cfa9a2c3
马洪贞 <hzma@alauda.io>
2022-11-28 11:17:39 +0800

    check if externalIds map is nil when add node as gw for centralized subnet

--- pkg/controller/node.go
+++ pkg/controller/node.go
@@ -1211,6 +1211,9 @@ func (c *Controller) addPolicyRouteForCentralizedSubnetOnNode(nodeName, nodeIP s
 						continue
 					}
 					nextHops = append(nextHops, nextHop)
+					if nameIpMap == nil {
+						nameIpMap = make(map[string]string, 1)
+					}
 					nameIpMap[nodeName] = nextHop
 
 					if err = c.updatePolicyRouteForCentralizedSubnet(subnet.Name, cidrBlock, nextHops, nameIpMap); err != nil {
