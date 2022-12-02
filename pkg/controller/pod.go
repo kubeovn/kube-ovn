ddcbd69f4af967072de7af1515f39ce4af116fd1
changluyi <clyi@alauda.io>
2022-12-02 11:15:40 +0800

    refactor

--- pkg/controller/pod.go
+++ pkg/controller/pod.go
@@ -1297,7 +1297,7 @@ func (c *Controller) acquireAddress(pod *v1.Pod, podNet *kubeovnNet) (string, st
 		for _, net := range nsNets {
 			for _, staticIPs := range ipPool {
 				for _, staticIP := range strings.Split(staticIPs, ",") {
-					if assignedPod, isAssigned := c.ipam.IsIPAssignedToOtherPod(staticIP, net.Subnet.Name, key); isAssigned {
+					if assignedPod, ok := c.ipam.IsIPAssignedToOtherPod(staticIP, net.Subnet.Name, key); ok {
 						klog.Errorf("static address %s for %s has been assigned to %", staticIP, key, assignedPod)
 						continue
 					}
