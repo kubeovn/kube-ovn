69ff0eedf7ac2fb16c6719542a88c1f4ecbb5c9a
张祖建 <zhangzujian.7@gmail.com>
2022-12-08 12:17:36 +0800

    some optimization for provider network status update (#2135)

--- pkg/controller/provider-network.go
+++ pkg/controller/provider-network.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/kubeovn/kube-ovn/pkg/util"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/klog/v2"
+
+	"github.com/kubeovn/kube-ovn/pkg/util"
 )
 
 func (c *Controller) resyncProviderNetworkStatus() {
-	klog.Infof("start to sync ProviderNetwork status")
+	klog.V(3).Infof("start to sync ProviderNetwork status")
 	pns, err := c.providerNetworksLister.List(labels.Everything())
 	if err != nil {
 		klog.Errorf("failed to list provider network: %v", err)
@@ -24,52 +26,73 @@ func (c *Controller) resyncProviderNetworkStatus() {
 		return
 	}
 
+	pods, err := c.podsLister.Pods("").List(labels.Set{"app": "kube-ovn-cni"}.AsSelector())
+	if err != nil {
+		klog.Errorf("failed to list kube-ovn-cni pods: %v", err)
+		return
+	}
+
+	podMap := make(map[string]*corev1.Pod, len(pods))
+	for _, pod := range pods {
+		podMap[pod.Spec.NodeName] = pod
+	}
+
 	for _, cachedPn := range pns {
 		pn := cachedPn.DeepCopy()
 		var readyNodes, notReadyNodes, expectNodes []string
+		pnReadyAnnotation := fmt.Sprintf(util.ProviderNetworkReadyTemplate, pn.Name)
+		pnErrMsgAnnotation := fmt.Sprintf(util.ProviderNetworkErrMessageTemplate, pn.Name)
+
+		var conditionsUpdated bool
 		for _, node := range nodes {
 			if util.ContainsString(pn.Spec.ExcludeNodes, node.Name) {
-				pn.Status.RemoveNodeConditions(node.Name)
+				if pn.Status.RemoveNodeConditions(node.Name) {
+					conditionsUpdated = true
+				}
 				continue
 			}
-			if node.Labels[fmt.Sprintf(util.ProviderNetworkReadyTemplate, pn.Name)] == "true" {
-				pn.Status.SetNodeReady(node.Name, "InitOVSBridgeSucceeded", "")
+			if node.Labels[pnReadyAnnotation] == "true" {
+				if pn.Status.SetNodeReady(node.Name, "InitOVSBridgeSucceeded", "") {
+					conditionsUpdated = true
+				}
 				readyNodes = append(readyNodes, node.Name)
 			} else {
-				pods, err := c.config.KubeClient.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
-					LabelSelector: "app=kube-ovn-cni",
-					FieldSelector: fmt.Sprintf("spec.nodeName=%s", node.Name),
-				})
-				if err != nil {
-					klog.Errorf("failed to list pod: %v", err)
-					continue
+				var errMsg string
+				if pod := podMap[node.Name]; pod == nil {
+					errMsg = fmt.Sprintf("kube-ovn-cni pod on node %s not found", node.Name)
+					klog.Error(errMsg)
+				} else {
+					if len(pod.Annotations) != 0 {
+						errMsg = pod.Annotations[pnErrMsgAnnotation]
+					}
+					if errMsg == "" {
+						errMsg = "unknown error"
+					}
 				}
 
-				var errMsg string
-				if len(pods.Items) == 1 && pods.Items[0].Annotations != nil {
-					errMsg = pods.Items[0].Annotations[fmt.Sprintf(util.ProviderNetworkErrMessageTemplate, pn.Name)]
+				if pn.Status.SetNodeNotReady(node.Name, "InitOVSBridgeFailed", errMsg) {
+					conditionsUpdated = true
 				}
-				pn.Status.SetNodeNotReady(node.Name, "InitOVSBridgeFailed", errMsg)
 				notReadyNodes = append(notReadyNodes, node.Name)
 			}
 		}
 
 		expectNodes = append(readyNodes, notReadyNodes...)
-		conditionsChange := false
 		for _, c := range pn.Status.Conditions {
 			if !util.ContainsString(expectNodes, c.Node) {
-				pn.Status.RemoveNodeConditions(c.Node)
-				conditionsChange = true
+				if pn.Status.RemoveNodeConditions(c.Node) {
+					conditionsUpdated = true
+				}
 			}
 		}
 
-		if conditionsChange || len(util.DiffStringSlice(pn.Status.ReadyNodes, readyNodes)) != 0 ||
+		if conditionsUpdated || len(util.DiffStringSlice(pn.Status.ReadyNodes, readyNodes)) != 0 ||
 			len(util.DiffStringSlice(pn.Status.NotReadyNodes, notReadyNodes)) != 0 {
 			pn.Status.ReadyNodes = readyNodes
 			pn.Status.NotReadyNodes = notReadyNodes
 			pn.Status.Ready = (len(notReadyNodes) == 0)
 			if _, err = c.config.KubeOvnClient.KubeovnV1().ProviderNetworks().UpdateStatus(context.Background(), pn, metav1.UpdateOptions{}); err != nil {
-				klog.Errorf("failed to update provider network %s: %v", pn.Name, err)
+				klog.Errorf("failed to update status of provider network %s: %v", pn.Name, err)
 			}
 		}
 	}
