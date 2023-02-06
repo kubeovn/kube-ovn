57f2c17d3c1d8584efbb4ad1544f0931df7d6d1c
changluyi <clyi@alauda.io>
2023-02-06 17:33:58 +0800

    fix u2o code err

--- pkg/daemon/ovs_linux.go
+++ pkg/daemon/ovs_linux.go
@@ -346,9 +346,10 @@ func configureContainerNic(nicName, ifName string, ipAddr, gateway string, isDef
 					if err := waitNetworkReady(ifName, ipAddr, u2oInterconnectionIP, false, true); err != nil {
 						return err
 					}
-				}
-				if err := waitNetworkReady(ifName, ipAddr, u2oInterconnectionIP, false, true); err != nil {
-					return err
+				} else {
+					if err := waitNetworkReady(nicName, ipAddr, u2oInterconnectionIP, false, true); err != nil {
+						return err
+					}
 				}
 			}
 
