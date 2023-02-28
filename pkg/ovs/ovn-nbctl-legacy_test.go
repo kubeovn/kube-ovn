360674f2a174dbd5d23b03697e6bb2eec04eef7e
Daviddcc <david.casier@aevoo.fr>
2023-02-28 13:50:42 +0100

    Update ovn-nbctl-legacy_test.go

--- pkg/ovs/ovn-nbctl-legacy_test.go
+++ pkg/ovs/ovn-nbctl-legacy_test.go
@@ -21,13 +21,14 @@ func Test_parseLrRouteListOutput(t *testing.T) {
 	output = `IPv6 Routes
     fc00:f853:ccd:e793::2            fd00:100:64::2 dst-ip
     fc00:f853:ccd:e793::3            fd00:100:64::3 dst-ip
+    fc00:f853:ccd:e793::4            fd00:100:64::4444 dst-ip
             fd00:10:16::2            fd00:100:64::3 src-ip
             fd00:10:16::d            fd00:100:64::2 src-ip
          fd00:11:15::/112            fd00:100:64::2 src-ip ecmp
          fd00:11:15::/112            fd00:100:64::3 src-ip ecmp`
 	routeList, err = parseLrRouteListOutput(output)
 	ast.Nil(err)
-	ast.Equal(6, len(routeList))
+	ast.Equal(7, len(routeList))
 }
 
 func Test_parseLrPolicyRouteListOutput(t *testing.T) {
