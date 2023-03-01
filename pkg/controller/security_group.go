d244551962ea359dfc1055f2496958ce30519f17
hzma <hzma@alauda.io>
2023-03-01 09:18:55 +0800

    update ipv6 security-group remote group name (#2389)

--- pkg/controller/security_group.go
+++ pkg/controller/security_group.go
@@ -364,6 +364,9 @@ func (c *Controller) syncSgLogicalPort(key string) error {
 		klog.Errorf("failed to find logical port, %v", err)
 		return err
 	}
+	if len(results) == 0 {
+		return nil
+	}
 
 	var v4s, v6s []string
 	var ports []string
