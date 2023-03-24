9d3d175e64a327decc05beb0fa2f3a5a4d839caf
zhangzujian <zhangzujian.7@gmail.com>
2023-03-24 10:06:28 +0800

    controller: fix apiserver connection timeout on startup

--- pkg/util/k8s.go
+++ pkg/util/k8s.go
@@ -1,10 +1,38 @@
 package util
 
 import (
+	"fmt"
+	"net"
+	"net/url"
 	"strings"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
 )
+
+func DialApiServer(host string) error {
+	u, err := url.Parse(host)
+	if err != nil {
+		return fmt.Errorf("failed to parse host %q: %v", host, err)
+	}
+
+	address := net.JoinHostPort(u.Hostname(), u.Port())
+	timer := time.NewTimer(3 * time.Second)
+	for i := 0; i < 10; i++ {
+		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+		if err == nil {
+			klog.Infof("succeeded to connected to apiserver %q", host)
+			conn.Close()
+			return nil
+		}
+		klog.Warningf("failed to dial %s: %v", address, err)
+		<-timer.C
+		timer.Reset(3 * time.Second)
+	}
+
+	return fmt.Errorf("timed out connecting to apiserver %q", host)
+}
 
 func GetNodeInternalIP(node v1.Node) (ipv4, ipv6 string) {
 	var ips []string
