d1711acd2d27b74815b2ef22a985c64b3f63d58f
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-05-04 16:44:13 +0800

    refresh nat gw image before using it (#2743)

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1012,10 +1012,6 @@ func (c *Controller) startWorkers(ctx context.Context) {
 	}, time.Second, ctx.Done())
 
 	go wait.Until(func() {
-		c.resyncVpcNatConfig()
-	}, time.Second, ctx.Done())
-
-	go wait.Until(func() {
 		c.resyncVpcNatGwConfig()
 	}, time.Second, ctx.Done())
 
