f3833f91b42d46692f618ec0b3f4976e95b85739
wujixin <599230270@qq.com>
2023-07-12 11:06:05 +0800

    feat:  suport kubevirt nic hotplug (#3013)
    Signed-off-by: wujixin <wujix@yealink.com>
    Co-authored-by: wujixin <wujix@yealink.com>

--- pkg/daemon/ovs_linux.go
+++ pkg/daemon/ovs_linux.go
@@ -191,6 +191,11 @@ func generateNicName(containerID, ifname string) (string, string) {
 	if ifname == "eth0" {
 		return fmt.Sprintf("%s_h", containerID[0:12]), fmt.Sprintf("%s_c", containerID[0:12])
 	}
+	// The nic name is 14 length and have prefix pod in the Kubevirt v1.0.0
+	if strings.HasPrefix(ifname, "pod") && len(ifname) == 14 {
+		ifname = ifname[3 : len(ifname)-4]
+		return fmt.Sprintf("%s_%s_h", containerID[0:12-len(ifname)], ifname), fmt.Sprintf("%s_%s_c", containerID[0:12-len(ifname)], ifname)
+	}
 	return fmt.Sprintf("%s_%s_h", containerID[0:12-len(ifname)], ifname), fmt.Sprintf("%s_%s_c", containerID[0:12-len(ifname)], ifname)
 }
 
