03f94a5279759d506a9d8397f6704d127e46aca0
马洪贞 <hzma@alauda.io>
2023-07-19 10:35:44 +0800

    Revert "prepare for next release"
    This reverts commit 6b0cc730c9af992ff22598ef6194ff59eb2e1a35.

--- dist/images/ovn-ic-db-docker.sh
+++ dist/images/ovn-ic-db-docker.sh
@@ -1,1 +1,1 @@
-docker run -d --network=host -v /etc/ovn/:/etc/ovn -v /var/run/ovn:/var/run/ovn -v /var/log/ovn:/var/log/ovn kubeovn/kube-ovn:v1.11.9 bash start-ic-db.sh
+docker run -d --network=host -v /etc/ovn/:/etc/ovn -v /var/run/ovn:/var/run/ovn -v /var/log/ovn:/var/log/ovn kubeovn/kube-ovn:v1.11.8 bash start-ic-db.sh
