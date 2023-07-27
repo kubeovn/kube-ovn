da1648ccd64c3436669d643e2d7a1b474d1d7cd9
张祖建 <zhangzujian.7@gmail.com>
2023-07-27 23:28:04 +0800

    fix go fmt

--- pkg/speaker/subnet.go
+++ pkg/speaker/subnet.go
@@ -96,7 +96,6 @@ func (c *Controller) syncSubnetRoutes() {
 		}
 	}
 
-
 	klog.V(5).Infof("expected announce ipv4 routes %v\n,ipv6 route %v", bgpExpected[kubeovnv1.ProtocolIPv4], bgpExpected[kubeovnv1.ProtocolIPv6])
 
 	fn := func(d *bgpapi.Destination) {
