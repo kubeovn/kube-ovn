1a720192ddbf3639bd5371a4ffbd604405da3cb3
张祖建 <zhangzujian.7@gmail.com>
2023-08-09 09:56:15 +0800

    add e2e test for ovn db recover (#3118)

--- test/e2e/framework/exec_utils.go
+++ test/e2e/framework/exec_utils.go
@@ -12,23 +12,23 @@ import (
 )
 
 // ExecCommandInContainer executes a command in the specified container.
-func ExecCommandInContainer(f *Framework, podName, containerName string, cmd ...string) (string, string, error) {
-	return util.ExecuteCommandInContainer(f.ClientSet, f.ClientConfig(), f.Namespace.Name, podName, containerName, cmd...)
+func ExecCommandInContainer(f *Framework, namespace, pod, container string, cmd ...string) (string, string, error) {
+	return util.ExecuteCommandInContainer(f.ClientSet, f.ClientConfig(), namespace, pod, container, cmd...)
 }
 
 // ExecShellInContainer executes the specified command on the pod's container.
-func ExecShellInContainer(f *Framework, podName, containerName string, cmd string) (string, string, error) {
-	return ExecCommandInContainer(f, podName, containerName, "/bin/sh", "-c", cmd)
+func ExecShellInContainer(f *Framework, namespace, pod, container, cmd string) (string, string, error) {
+	return ExecCommandInContainer(f, namespace, pod, container, "/bin/sh", "-c", cmd)
 }
 
-func execCommandInPod(ctx context.Context, f *Framework, podName string, cmd ...string) (string, string, error) {
-	pod, err := f.PodClient().Get(ctx, podName, metav1.GetOptions{})
-	framework.ExpectNoError(err, "failed to get pod %v", podName)
-	gomega.Expect(pod.Spec.Containers).NotTo(gomega.BeEmpty())
-	return ExecCommandInContainer(f, podName, pod.Spec.Containers[0].Name, cmd...)
+func execCommandInPod(ctx context.Context, f *Framework, namespace, pod string, cmd ...string) (string, string, error) {
+	p, err := f.PodClientNS(namespace).Get(ctx, pod, metav1.GetOptions{})
+	framework.ExpectNoError(err, "failed to get pod %s/%s", namespace, pod)
+	gomega.Expect(p.Spec.Containers).NotTo(gomega.BeEmpty())
+	return ExecCommandInContainer(f, namespace, pod, p.Spec.Containers[0].Name, cmd...)
 }
 
 // ExecShellInPod executes the specified command on the pod.
-func ExecShellInPod(ctx context.Context, f *Framework, podName string, cmd string) (string, string, error) {
-	return execCommandInPod(ctx, f, podName, "/bin/sh", "-c", cmd)
+func ExecShellInPod(ctx context.Context, f *Framework, namespace, pod, cmd string) (string, string, error) {
+	return execCommandInPod(ctx, f, namespace, pod, "/bin/sh", "-c", cmd)
 }
