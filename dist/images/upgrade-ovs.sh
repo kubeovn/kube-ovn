f3b646053361002526e6ae4fb4ad3bf3f37cbab6
张祖建 <zhangzujian.7@gmail.com>
2023-09-04 14:00:55 +0800

    chart: fix ovs-ovn upgrade (#3164)
    Signed-off-by: 张祖建 <zhangzujian.7@gmail.com>

--- dist/images/upgrade-ovs.sh
+++ dist/images/upgrade-ovs.sh
@@ -5,9 +5,9 @@ set -e
 POD_NAMESPACE=${POD_NAMESPACE:-kube-system}
 
 dsGenVer=`kubectl -n $POD_NAMESPACE get ds ovs-ovn -o jsonpath={.metadata.generation}`
+kubectl -n $POD_NAMESPACE delete pod -l app=ovs,pod-template-generation!=$dsGenVer
+
 for node in `kubectl get node -o jsonpath='{.items[*].metadata.name}'`; do
-  # delete pod with old version
-  kubectl -n $POD_NAMESPACE delete pod -l app=ovs,pod-template-generation!=$dsGenVer --field-selector spec.nodeName=$node
   # wait the pod with new version to be created and delete it
   while true; do
     pod=`kubectl -n $POD_NAMESPACE get pod -l app=ovs,pod-template-generation=$dsGenVer --field-selector spec.nodeName=$node -o name`
