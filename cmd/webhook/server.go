a451c7ab7d241064e7f50cace8a4d1ef9d82deb6
张祖建 <zhangzujian.7@gmail.com>
2023-10-08 13:44:19 +0800

    webhook: use dedicated port for health probe
    Signed-off-by: 张祖建 <zhangzujian.7@gmail.com>

--- cmd/webhook/server.go
+++ cmd/webhook/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/spf13/pflag"
 	appsv1 "k8s.io/api/apps/v1"
@@ -10,6 +11,7 @@ import (
 	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	ctrlwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -18,6 +20,8 @@ import (
 	ovnwebhook "github.com/kubeovn/kube-ovn/pkg/webhook"
 	"github.com/kubeovn/kube-ovn/versions"
 )
+
+const healthProbePort = 8080
 
 const hookServerCertDir = "/tmp/k8s-webhook-server/serving-certs"
 
@@ -73,6 +77,7 @@ func main() {
 		Metrics: metricsserver.Options{
 			BindAddress: "0",
 		},
+		HealthProbeBindAddress: util.JoinHostPort(os.Getenv("POD_IP"), healthProbePort),
 	})
 	if err != nil {
 		panic(err)
@@ -88,6 +93,13 @@ func main() {
 	hookServer.Register("/validating", &ctrlwebhook.Admission{Handler: validatingHook})
 
 	if err := mgr.Add(hookServer); err != nil {
+		panic(err)
+	}
+
+	if err = mgr.AddHealthzCheck("liveness probe", healthz.Ping); err != nil {
+		panic(err)
+	}
+	if err = mgr.AddReadyzCheck("readiness probe", healthz.Ping); err != nil {
 		panic(err)
 	}
 
