cf3fec28e0690b3c8ed7f299c6b4e52d54e2cde4
Changlu Yi <clyi@alauda.io>
2023-11-06 11:34:32 +0800

    add kube-ovn-controller nodeAffinity prefer not on ic gateway
    Signed-off-by: Changlu Yi <clyi@alauda.io>

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -3919,6 +3919,15 @@ spec:
         - key: CriticalAddonsOnly
           operator: Exists
       affinity:
+        nodeAffinity:
+          preferredDuringSchedulingIgnoredDuringExecution:
+          - preference:
+              matchExpressions:
+              - key: "ovn.kubernetes.io/ic-gw"
+                operator: NotIn
+                values:
+                - "true"
+            weight: 100
         podAntiAffinity:
           requiredDuringSchedulingIgnoredDuringExecution:
             - labelSelector:
