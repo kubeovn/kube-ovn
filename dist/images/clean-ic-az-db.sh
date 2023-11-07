1ee06f7f852498e3650c82a72c8ed2636ab52735
Changlu Yi <clyi@alauda.io>
2023-11-07 17:28:54 +0800

    add detail info
    Signed-off-by: Changlu Yi <clyi@alauda.io>

--- dist/images/clean-ic-az-db.sh
+++ dist/images/clean-ic-az-db.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-   echo "Usage: $0 <availability_zone_uuid> "
-   echo "   eg: ./clean-ic-az-db.sh 2106a2d8-f6d4-4645-b05a-59417556eccb "
+if [ "$#" -ne 2 ]; then
+   echo "use method $0 {az|node} {azName|nodeName}"
+   echo "   eg: ./clean-ic-az-db.sh az az1"
+   echo "   eg: ./clean-ic-az-db.sh node kube-ovn-worker; it will delete all resource of az that the node belong to"
    exit 1
 fi
 
-availability_zone=$1
+filter_type=$1
+filter_value=$2
+availability_zone_uuid=
 
-if ! ovn-ic-sbctl get availability_zone $availability_zone name >/dev/null 2>&1; then
-   echo "Availability zone $availability_zone not found."
+if [ "$filter_type" != "az" ] && [ "$filter_type" != "node" ]; then
+  echo "filter_type should be az or node."
+  exit 1
+fi
+
+if [ "$filter_type" == "az" ]; then
+  availability_zone_uuid=$(ovn-ic-sbctl --columns=_uuid find availability_zone name=$filter_value | awk '{print $3}')
+fi
+
+echo $availability_zone_uuid
+
+if [ "$filter_type" == "node" ]; then
+  availability_zone_uuid=$(ovn-ic-sbctl --columns=availability_zone find gateway hostname=$filter_value | awk '{print $3}')
+fi
+
+if ! ovn-ic-sbctl get availability_zone $availability_zone_uuid name >/dev/null 2>&1; then
+   echo "Availability zone $availability_zone_uuid not found."
    exit 1
 fi
 
 resource_types=("Gateway" "Route" "Port_Binding")
 
 for resource_type in "${resource_types[@]}"; do
-   uuid_array=($(ovn-ic-sbctl --columns=_uuid find $resource_type availability_zone=$availability_zone | awk '{print $3}'))
+   uuid_array=($(ovn-ic-sbctl --columns=_uuid find $resource_type availability_zone=$availability_zone_uuid | awk '{print $3}'))
 
    for uuid in "${uuid_array[@]}"; do
       ovn-ic-sbctl destroy $resource_type $uuid
@@ -24,6 +42,6 @@ for resource_type in "${resource_types[@]}"; do
    done
 done
 
-ovn-ic-sbctl destroy availability_zone $availability_zone
-echo "Destroyed availability_zone: $availability_zone"
+ovn-ic-sbctl destroy availability_zone $availability_zone_uuid
+echo "Destroyed availability_zone: $availability_zone_uuid"
 
