c13637dc78f0a7a93c8a7e060c80a20c4db6020f
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-12-01 11:27:05 +0800

    schedule kube-ovn-controller on the kube-ovn-master node (#3479)
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -4040,6 +4040,7 @@ spec:
               memory: 1Gi
       nodeSelector:
         kubernetes.io/os: "linux"
+        kube-ovn/role: master
       volumes:
         - name: localtime
           hostPath:
