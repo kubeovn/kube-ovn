43c4c2a195da51394354864c2ceabbcbe0f06a61
changluyi <47097611+changluyi@users.noreply.github.com>
2023-12-05 13:17:25 +0800

    iptables drop invalid rst (#3484)
    * iptables drop invalid rst
    
    Signed-off-by: changluyi <clyi@alauda.io>
    
    * Update uninstall.sh
    
    ---------
    
    Signed-off-by: changluyi <clyi@alauda.io>

--- dist/images/uninstall.sh
+++ dist/images/uninstall.sh
@@ -30,6 +30,8 @@ iptables -t mangle -F OVN-PREROUTING
 iptables -t mangle -X OVN-PREROUTING
 iptables -t mangle -F OVN-OUTPUT
 iptables -t mangle -X OVN-OUTPUT
+iptables -t mangle -F OVN-POSTROUTING
+iptables -t mangle -X OVN-POSTROUTING
 
 sleep 1
 
@@ -69,6 +71,8 @@ ip6tables -t mangle -F OVN-PREROUTING
 ip6tables -t mangle -X OVN-PREROUTING
 ip6tables -t mangle -F OVN-OUTPUT
 ip6tables -t mangle -X OVN-OUTPUT
+ip6tables -t mangle -F OVN-POSTROUTING
+ip6tables -t mangle -X OVN-POSTROUTING
 
 sleep 1
 
