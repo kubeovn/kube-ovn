2a499b424f19af0ae7246f989c0a21f1773949ab
张祖建 <zhangzujian.7@gmail.com>
2023-12-15 09:34:41 +0800

    fix chassis gc (#3525)
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/controller/gc.go
+++ pkg/controller/gc.go
@@ -705,7 +705,7 @@ func (c *Controller) gcStaticRoute() error {
 
 func (c *Controller) gcChassis() error {
 	klog.Infof("start to gc chassis")
-	chassises, err := c.OVNSbClient.GetKubeOvnChassisses()
+	chassises, err := c.OVNSbClient.ListChassis()
 	if err != nil {
 		klog.Errorf("failed to get all chassis, %v", err)
 	}
@@ -727,6 +727,7 @@ func (c *Controller) gcChassis() error {
 		if hostname, exist := chassisNodes[chassisName]; exist {
 			if hostname == node.Name {
 				// node is alive, matched chassis should be alive
+				delete(chassisNodes, chassisName)
 				continue
 			}
 			// maybe node name changed, delete chassis
@@ -737,6 +738,15 @@ func (c *Controller) gcChassis() error {
 			}
 		}
 	}
+
+	for chassisName, hostname := range chassisNodes {
+		klog.Infof("gc node %s chassis %s", hostname, chassisName)
+		if err := c.OVNSbClient.DeleteChassis(chassisName); err != nil {
+			klog.Errorf("failed to delete node %s chassis %s %v", hostname, chassisName, err)
+			return err
+		}
+	}
+
 	return nil
 }
 
