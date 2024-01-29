8504fcaf44cbe33e18854c7ec89ea13bf86120a1
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-01-29 14:40:01 +0800

    simplicify
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- pkg/util/net.go
+++ pkg/util/net.go
@@ -293,12 +293,6 @@ func GetStringIP(v4IP, v6IP string) string {
 	if IsValidIP(v6IP) {
 		ipList = append(ipList, v6IP)
 	}
-	if len(ipList) == 0 {
-		return ""
-	}
-	if len(ipList) == 1 {
-		return ipList[0]
-	}
 	return strings.Join(ipList, ",")
 }
 
