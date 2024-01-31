1794ab8779951a80c6137b7029fd88625389c92b
Changlu Yi <clyi@alauda.io>
2024-01-31 16:15:37 +0800

    Compatible with controller deployment methods before kube-ovn 1.11.16
    Signed-off-by: Changlu Yi <clyi@alauda.io>

--- dist/images/start-ic-db.sh
+++ dist/images/start-ic-db.sh
@@ -215,7 +215,12 @@ if [[ $ENABLE_OVN_LEADER_CHECK == "true" ]]; then
     chmod 600 /etc/ovn/*
     /kube-ovn/kube-ovn-leader-checker --probeInterval=${OVN_LEADER_PROBE_INTERVAL} --isICDBServer=true
 else
-
+    # Compatible with controller deployment methods before kube-ovn 1.11.16
+    TS_NAME=${TS_NAME:-ts}
+    PROTOCOL=${PROTOCOL:-ipv4}
+    TS_CIDR=${TS_CIDR:-169.254.100.0/24}
+    ovn-ic-nbctl --may-exist ts-add "$TS_NAME"
+    ovn-ic-nbctl set Transit_Switch ts external_ids:subnet="$TS_CIDR"
     tail --follow=name --retry /var/log/ovn/ovsdb-server-ic-nb.log
 fi
 
