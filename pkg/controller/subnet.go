b5a3a6d11d58107db3e5a4510cbcc720e51e5b03
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-02-06 15:54:08 +0800

    ip trigger subnet delete
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- pkg/controller/subnet.go
+++ pkg/controller/subnet.go
@@ -823,6 +823,23 @@ func (c *Controller) handleUpdateSubnetStatus(key string) error {
 		klog.Error(err)
 		return err
 	}
+
+	if !subnet.DeletionTimestamp.IsZero() {
+		// subnet is being deleted
+		cachedSubnet, err = c.subnetsLister.Get(key)
+		subnet = cachedSubnet.DeepCopy()
+		if err != nil {
+			if k8serrors.IsNotFound(err) {
+				return nil
+			}
+			return err
+		}
+		_, err = c.handleSubnetFinalizer(subnet)
+		if err != nil {
+			klog.Errorf("faile to handle finalizer for subnet %s, %v", key, err)
+			return err
+		}
+	}
 	return nil
 }
 
