b45c8339ab370c3c68d532ebbe475781d982251c
oilbeater <liumengxinfly@gmail.com>
2024-02-21 05:58:04 +0000

    release v1.12.5

---
+++ hack/release.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "check status of last commit build"
+commit=$(git rev-parse HEAD)
+check_status=$(curl https://api.github.com/repos/kubeovn/kube-ovn/commits/$commit/check-runs)
+if ! echo $check_status | grep -q '"conclusion": "failure"'; then
+    echo "last commit build successed"
+else
+    echo "last commit build failed"
+fi
+
+
+echo "tag and push image"
+VERSION=$(cat VERSION)
+docker manifest rm kubeovn/kube-ovn:${VERSION}
+docker manifest rm kubeovn/vpc-nat-gateway:${VERSION}
+
+docker pull kubeovn/kube-ovn:${VERSION}-x86
+docker pull kubeovn/kube-ovn:${VERSION}-arm
+docker pull kubeovn/vpc-nat-gateway:${VERSION}-x86
+docker pull kubeovn/vpc-nat-gateway:${VERSION}-arm
+
+docker manifest create kubeovn/kube-ovn:${VERSION} kubeovn/kube-ovn:${VERSION}-x86 kubeovn/kube-ovn:${VERSION}-arm
+docker manifest create kubeovn/vpc-nat-gateway:${VERSION} kubeovn/vpc-nat-gateway:${VERSION}-x86 kubeovn/vpc-nat-gateway:${VERSION}-arm
+
+docker manifest push kubeovn/kube-ovn:${VERSION}
+docker manifest push kubeovn/vpc-nat-gateway:${VERSION}
+
+echo "modify tag in install.sh and values.yaml"
+sed -i '/^VERSION=/c\VERSION="'"${VERSION}"'"' dist/images/install.sh
+sed -i 's/tag:\ .*/tag:\ '"${VERSION}"'/' charts/kube-ovn/values.yaml
+
+echo "commit, tag and push"
+git add dist/images/install.sh
+git add charts/kube-ovn/values.yaml
+git commit -m "release ${VERSION}"
+git tag ${VERSION}
+git push
+git push origin --tags
+
+echo "modify version to next patch number"
+NEXT_VERSION=$(cat VERSION | awk -F '.' '{print $1"."$2"."$3+1}')
+echo ${NEXT_VERSION} > VERSION
+git add VERSION
+git commit -m "prepare for next release"
+git push
+
+echo "draft a release"
+gh release create $VERSION --draft
+
+echo "Need to modify the doc version number manually"
