b5f7a6348bdf29a1edd6a7c2962f6feece89f4fa
hzma <hzma@alauda.io>
2024-02-26 16:59:31 +0800

    refactor ovn clusterrole (#3755)
    Signed-off-by: 马洪贞 <hzma@alauda.io>

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -2831,8 +2831,6 @@ rules:
       - patch
   - apiGroups:
       - ""
-      - networking.k8s.io
-      - apps
     resources:
       - services
       - endpoints
@@ -2923,17 +2921,28 @@ rules:
       - ""
     resources:
       - pods
-      - pods/exec
       - namespaces
-      - nodes
-      - configmaps
     verbs:
-      - create
       - get
       - list
+      - patch
       - watch
+  - apiGroups:
+      - ""
+    resources:
+      - nodes
+    verbs:
+      - get
+      - list
       - patch
       - update
+      - watch
+  - apiGroups:
+      - ""
+    resources:
+      - pods/exec
+    verbs:
+      - create
   - apiGroups:
       - "k8s.cni.cncf.io"
     resources:
@@ -2943,40 +2952,53 @@ rules:
   - apiGroups:
       - ""
       - networking.k8s.io
-      - apps
     resources:
       - networkpolicies
-      - daemonsets
+      - configmaps
     verbs:
       - get
       - list
       - watch
   - apiGroups:
-      - ""
       - apps
     resources:
-      - services/status
+      - daemonsets
     verbs:
-      - update
+      - get
   - apiGroups:
       - ""
-      - networking.k8s.io
-      - apps
-      - extensions
     resources:
       - services
+      - services/status
+    verbs:
+      - get
+      - list
+      - update
+      - create
+      - delete
+      - watch
+  - apiGroups:
+      - ""
+    resources:
       - endpoints
+    verbs:
+      - create
+      - update
+      - get
+      - list
+      - watch
+  - apiGroups:
+      - apps
+    resources:
       - statefulsets
       - deployments
       - deployments/scale
     verbs:
+      - get
+      - list
       - create
       - delete
       - update
-      - patch
-      - get
-      - list
-      - watch
   - apiGroups:
       - ""
     resources:
@@ -3031,29 +3053,34 @@ metadata:
 rules:
   - apiGroups:
       - "kubeovn.io"
+      - ""
     resources:
       - subnets
       - provider-networks
-      - ovn-eips
-      - ovn-eips/status
-      - ips
+      - pods
     verbs:
       - get
       - list
-      - patch
-      - update
       - watch
   - apiGroups:
       - ""
+      - "kubeovn.io"
     resources:
-      - pods
+      - ovn-eips
+      - ovn-eips/status
       - nodes
-      - configmaps
     verbs:
       - get
       - list
       - patch
       - watch
+  - apiGroups:
+      - "kubeovn.io"
+    resources:
+      - ips
+    verbs:
+      - get
+      - update
   - apiGroups:
       - ""
     resources:
@@ -3101,8 +3128,6 @@ rules:
       - get
       - list
   - apiGroups:
-      - ""
-      - networking.k8s.io
       - apps
     resources:
       - daemonsets
