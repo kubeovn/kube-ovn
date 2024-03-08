5d9dc0625b1a7123415ab2e3531eeaf6fa5b41da
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-03-08 16:25:22 +0800

    fix chart directory
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- pkg/controller/vpc_nat_gateway.go
+++ pkg/controller/vpc_nat_gateway.go
@@ -1086,39 +1086,3 @@ func (c *Controller) execNatGwQoSInPod(
 	}
 	return nil
 }
-
-func (c *Controller) initVpcNatGw() error {
-	klog.Infof("init all vpc nat gateways")
-	gws, err := c.vpcNatGatewayLister.List(labels.Everything())
-	if err != nil {
-		err = fmt.Errorf("failed to get vpc nat gw list, %v", err)
-		klog.Error(err)
-		return err
-	}
-	if len(gws) == 0 {
-		return nil
-	}
-
-	if vpcNatEnabled != "true" {
-		err := fmt.Errorf("iptables nat gw not enable")
-		klog.Warning(err)
-		return nil
-	}
-
-	for _, gw := range gws {
-		pod, err := c.getNatGwPod(gw.Name)
-		if err != nil {
-			// the nat gw maybe deleted
-			err := fmt.Errorf("failed to get nat gw %s pod: %v", gw.Name, err)
-			klog.Error(err)
-			continue
-		}
-		if vpcGwName, isVpcNatGw := pod.Annotations[util.VpcNatGatewayAnnotation]; isVpcNatGw {
-			if _, hasInit := pod.Annotations[util.VpcNatGatewayInitAnnotation]; hasInit {
-				return nil
-			}
-			c.initVpcNatGatewayQueue.Add(vpcGwName)
-		}
-	}
-	return nil
-}
