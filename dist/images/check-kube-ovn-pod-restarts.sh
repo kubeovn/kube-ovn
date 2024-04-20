708f9c5aad9269513d41ab7ba40ccd834eb1676d
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-04-20 06:46:14 +0000

    fix check
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- dist/images/check-kube-ovn-pod-restarts.sh
+++ dist/images/check-kube-ovn-pod-restarts.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-set -eux
 
-Namespace="kube-system"
-crashedPodsNum=$(kubectl get po -n "$Namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn" | awk '{print $3$4}' | grep -v -c "Running0")
+namespace="kube-system"
 
+# check if there are any crashed pods
+crashedPodsNum=$(kubectl get po -n "$namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn" | awk '{print $3$4}' | grep -v -c "Running0")
 if [ "$crashedPodsNum" -gt 0 ]; then
-  echo "some ovn pods are not running"
-  kubectl get po -n "$Namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn"
-  crashedPods=$(kubectl get po -n "$Namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn" |  awk '{print $1 " " $3$4}' | grep -v "Running0" | awk '{print $1}')
-  for crashedPod in $crashedPods; do
-    echo "kubectl logs -p -n $crashedPod | tail -n 100"
-    kubectl logs -p -n "$Namespace" "$crashedPod" | tail -n 100
-    echo "PLEASE CHECK THE ERROR LOGS ABOVE /|\ /|\ /|\ "
-  done
-  exit 1
+    echo "some ovn pods are not running"
+    kubectl get po -n "$namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn"
+    crashedPods=$(kubectl get po -n "$namespace" -o wide | grep -E "ovn-controller|ovn-pinger|ovn-monitor|ovn-cni|ovn-central|ovs-ovn" |  awk '{print $1 " " $3$4}' | grep -v "Running0" | awk '{print $1}')
+    for crashedPod in $crashedPods; do
+      echo "kubectl logs -p -n $crashedPod | tail -n 100"
+      kubectl logs -p -n "$namespace" "$crashedPod" | tail -n 100
+      echo "PLEASE CHECK THE ERROR LOGS ABOVE /|\ /|\ /|\ "
+    done
+
+    echo "some ovn pods crashed"
+    exit 1
+else
+    echo "all ovn pods are ok"
+    exit 0
 fi
