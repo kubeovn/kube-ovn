fcbf71c7a99c99cfb0002a7be1526456b7b26940
zhangzujian <zhangzujian.7@gmail.com>
2024-05-24 07:12:30 +0000

    fix exit code
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- hack/ci-check-crash.sh
+++ hack/ci-check-crash.sh
@@ -2,12 +2,16 @@
 
 namespace="kube-system"
 
+exit_code=0
 # check if there are any crashed pods
 for pod in `kubectl get pod -n $namespace -l component=network -o name`; do
   restartCount=`kubectl get -n $namespace $pod -o jsonpath='{.status.containerStatuses[0].restartCount}'`
   # TODO: get restart count for all containers
   if [ $restartCount -gt 0 ]; then
+    exit_code=1
     echo "$pod restarted $restartCount time(s). Logs of the previous instance:"
     kubectl logs -p -n $namespace $pod
   fi
 done
+
+exit $exit_code
