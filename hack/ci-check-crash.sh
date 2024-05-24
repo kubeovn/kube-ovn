40f6445dda255420b3e8c73a7966f7280982e704
zhangzujian <zhangzujian.7@gmail.com>
2024-05-24 06:56:52 +0000

    fix script
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- hack/ci-check-crash.sh
+++ hack/ci-check-crash.sh
@@ -3,11 +3,11 @@
 namespace="kube-system"
 
 # check if there are any crashed pods
-for pod in `kubectl get pod -n $namespace -l component=network -o`; do
+for pod in `kubectl get pod -n $namespace -l component=network -o name`; do
   restartCount=`kubectl get -n $namespace $pod -o jsonpath='{.status.containerStatuses[0].restartCount}'`
   # TODO: get restart count for all containers
   if [ $restartCount -gt 0 ]; then
-    echo "$pod restarted $restartCount times. Logs of the previous instance:"
+    echo "$pod restarted $restartCount time(s). Logs of the previous instance:"
     kubectl logs -p -n $namespace $pod
   fi
 done
