be7182c1f69cc394e0934613c088a45046a16bb0
Zhao Congqi <1229896069@qq.com>
2024-05-29 10:02:30 +0800

    Update pkg/util/strings_test.go
    Co-authored-by: bobz965 <jmdxjsjgcxy@gmail.com>
    Signed-off-by: zcq98 <zhaocongqi_yewu@cmss.chinamobile.com>

--- pkg/util/strings_test.go
+++ pkg/util/strings_test.go
@@ -61,7 +61,7 @@ func TestSha256Hash(t *testing.T) {
 			output: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
 		},
 		{
-			name:   "Non-empty input",
+			name:   "Non empty input",
 			input:  []byte("hello"),
 			output: "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
 		},
