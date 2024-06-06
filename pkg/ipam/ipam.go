426b5d0a633e5a5e2c15492b04f5b059f1921515
hzma <hzma@alauda.io>
2024-06-06 11:45:45 +0800

    fix ipam subnet concurrent map iteration and map write (#4126)
    Signed-off-by: 马洪贞 <hzma@alauda.io>

--- pkg/ipam/ipam.go
+++ pkg/ipam/ipam.go
@@ -331,7 +331,7 @@ func (ipam *IPAM) GetPodAddress(podName string) []*SubnetAddress {
 	defer ipam.mutex.RUnlock()
 	addresses := []*SubnetAddress{}
 	for _, subnet := range ipam.Subnets {
-		subnet.mutex.RLock()
+		subnet.Mutex.RLock()
 		for _, nicName := range subnet.PodToNicList[podName] {
 			v4IP, v6IP, mac, protocol := subnet.GetPodAddress(podName, nicName)
 			switch protocol {
@@ -344,7 +344,7 @@ func (ipam *IPAM) GetPodAddress(podName string) []*SubnetAddress {
 				addresses = append(addresses, &SubnetAddress{Subnet: subnet, IP: v6IP.String(), Mac: mac})
 			}
 		}
-		subnet.mutex.RUnlock()
+		subnet.Mutex.RUnlock()
 	}
 	return addresses
 }
