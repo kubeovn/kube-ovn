a3fd8d4c109e12dde62c46bc80f3fcc939c050ec
马洪贞 <hzma@alauda.io>
2024-06-13 11:43:40 +0800

    keep underlay subnet check same as before
    Signed-off-by: 马洪贞 <hzma@alauda.io>

--- pkg/daemon/controller.go
+++ pkg/daemon/controller.go
@@ -684,7 +684,7 @@ func (c *Controller) reconcileRouters(event subnetEvent) error {
 	cidrs := make([]string, 0, len(subnets)*2)
 	for _, subnet := range subnets {
 		// The route for overlay subnet cidr via ovn0 should not be deleted even though subnet.Status has changed to not ready
-		if subnet.Spec.Vpc != util.DefaultVpc || (subnet.Spec.Vlan != "" && !subnet.Spec.LogicalGateway && !subnet.Spec.U2OInterconnection) {
+		if subnet.Spec.Vlan != "" || subnet.Spec.Vpc != util.DefaultVpc {
 			continue
 		}
 
