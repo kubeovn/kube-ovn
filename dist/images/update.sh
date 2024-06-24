666994c2ba8adc5101a68b140fa6a1534604a155
zhangzujian <zhangzujian.7@gmail.com>
2024-06-24 03:19:14 +0000

    delete unused scripts
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- dist/images/update.sh
+++
@@ -1,4 +0,0 @@
-nbleader=$(kubectl -n kube-system get pods -l ovn-nb-leader=true -o jsonpath='{.items[*].metadata.name}')
-echo "leader is " "${nbleader}"
-kubectl -n kube-system exec -ti "${nbleader}" -- bash /kube-ovn/add-label.sh
-echo "update finished"
