da2057c21001c8d12e595a9a012eae4881952861
oilbeater <liumengxinfly@gmail.com>
2024-07-02 03:27:21 +0000

    fix vpcnatgw image is not synced
    Signed-off-by: oilbeater <liumengxinfly@gmail.com>

--- pkg/controller/vpc_nat.go
+++ pkg/controller/vpc_nat.go
@@ -10,19 +10,22 @@ import (
 
 var vpcNatImage = ""
 
-func (c *Controller) resyncVpcNatImage() error {
+func (c *Controller) resyncVpcNatImage() {
+	if vpcNatEnabled != "true" {
+		return
+	}
+
 	cm, err := c.configMapsLister.ConfigMaps(c.config.PodNamespace).Get(util.VpcNatConfig)
 	if err != nil {
 		err = fmt.Errorf("failed to get ovn-vpc-nat-config, %v", err)
 		klog.Error(err)
-		return err
+		return
 	}
 	image, exist := cm.Data["image"]
 	if !exist {
 		err = fmt.Errorf("%s should have image field", util.VpcNatConfig)
 		klog.Error(err)
-		return err
+		return
 	}
 	vpcNatImage = image
-	return nil
 }
