a638a62d3d28ceb3f2fdeb5467a830c0c31e347b
zhangzujian <zhangzujian.7@gmail.com>
2024-07-19 13:48:07 +0000

    fix dialing https server
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- cmd/controller_health_check/controller_health_check.go
+++ cmd/controller_health_check/controller_health_check.go
@@ -1,25 +1,49 @@
 package controller_health_check
 
 import (
-	"net"
+	"flag"
 	"os"
 	"time"
+
+	"github.com/spf13/pflag"
+	"k8s.io/klog/v2"
 
 	"github.com/kubeovn/kube-ovn/pkg/util"
 )
 
 func CmdMain() {
+	tls := pflag.Bool("tls", false, "Whether kube-ovn-controller uses TLS")
+
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+
+	// sync the glog and klog flags.
+	pflag.CommandLine.VisitAll(func(f1 *pflag.Flag) {
+		f2 := klogFlags.Lookup(f1.Name)
+		if f2 != nil {
+			value := f1.Value.String()
+			if err := f2.Value.Set(value); err != nil {
+				util.LogFatalAndExit(err, "failed to set pflag")
+			}
+		}
+	})
+
+	pflag.CommandLine.AddGoFlagSet(klogFlags)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
 	addr := "127.0.0.1:10660"
 	if os.Getenv("ENABLE_BIND_LOCAL_IP") == "true" {
 		addr = util.JoinHostPort(os.Getenv("POD_IP"), 10660)
 	}
 
-	conn, err := net.DialTimeout("tcp", addr, 3*time.Second)
-	if err != nil {
-		util.LogFatalAndExit(err, "failed to probe the socket")
+	if *tls {
+		addr = "tls://" + addr
+	} else {
+		addr = "tcp://" + addr
 	}
-	err = conn.Close()
-	if err != nil {
-		util.LogFatalAndExit(err, "failed to close connection")
+
+	if err := util.DialTCP(addr, time.Second, false); err != nil {
+		util.LogFatalAndExit(err, "failed to probe the socket")
 	}
 }
