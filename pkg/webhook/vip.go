2433416a581d51ab0be28ac60a1af913d8f1e2b3
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-08-07 16:55:06 +0800

    fix as lint
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- pkg/webhook/vip.go
+++ pkg/webhook/vip.go
@@ -10,7 +10,6 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/klog/v2"
 	ctrlwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
