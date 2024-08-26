5d001a9409a0d696b644dc967f3293bd2f7a6cfe
zhangzujian <zhangzujian.7@gmail.com>
2024-08-26 05:59:45 +0000

    cni: always enable ipv6
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

---
+++ cmd/cni/sysctl.go
@@ -0,0 +1,32 @@
+//go:build !windows
+// +build !windows
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/containernetworking/plugins/pkg/utils/sysctl"
+)
+
+// For docker version >=17.x the "none" network will disable ipv6 by default.
+// We have to enable ipv6 here to add v6 address and gateway.
+// See https://github.com/containernetworking/cni/issues/531
+func sysctlEnableIPv6(nsPath string) error {
+	return ns.WithNetNSPath(nsPath, func(_ ns.NetNS) error {
+		for _, conf := range [...]string{"all", "default"} {
+			name := fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", conf)
+			value, err := sysctl.Sysctl(name)
+			if err != nil {
+				return fmt.Errorf("failed to get sysctl variable %s: %w", name, err)
+			}
+			if value != "0" {
+				if _, err = sysctl.Sysctl(name, "0"); err != nil {
+					return fmt.Errorf("failed to set sysctl variable %s to 0: %w", name, err)
+				}
+			}
+		}
+		return nil
+	})
+}
