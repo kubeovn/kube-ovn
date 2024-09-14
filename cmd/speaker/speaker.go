11f29dcab370d41adece2e8e1d6e4c38214c6c9e
zhangzujian <zhangzujian.7@gmail.com>
2024-09-14 06:31:18 +0000

    fix speaker metrics
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- cmd/speaker/speaker.go
+++ cmd/speaker/speaker.go
@@ -1,15 +1,12 @@
 package speaker
 
 import (
-	"fmt"
-	"net/http"
-	"time"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"k8s.io/klog/v2"
 	"kernel.org/pub/linux/libs/security/libcap/cap"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 
+	"github.com/kubeovn/kube-ovn/pkg/metrics"
 	"github.com/kubeovn/kube-ovn/pkg/speaker"
 	"github.com/kubeovn/kube-ovn/pkg/util"
 	"github.com/kubeovn/kube-ovn/versions"
@@ -28,20 +25,15 @@ func CmdMain() {
 		util.LogFatalAndExit(err, "failed to parse config")
 	}
 
-	stopCh := signals.SetupSignalHandler().Done()
-	ctl := speaker.NewController(config)
-
+	ctrl.SetLogger(klog.NewKlogr())
+	ctx := signals.SetupSignalHandler()
 	go func() {
-		http.Handle("/metrics", promhttp.Handler())
-
-		// conform to Gosec G114
-		// https://github.com/securego/gosec#available-rules
-		server := &http.Server{
-			Addr:              fmt.Sprintf("0.0.0.0:%d", config.PprofPort),
-			ReadHeaderTimeout: 3 * time.Second,
+		metrics.InitKlogMetrics()
+		if err = metrics.Run(ctx, nil, util.JoinHostPort("0.0.0.0", config.PprofPort), false, false); err != nil {
+			util.LogFatalAndExit(err, "failed to run metrics server")
 		}
-		util.LogFatalAndExit(server.ListenAndServe(), "failed to listen and serve on %s", server.Addr)
+		<-ctx.Done()
 	}()
 
-	ctl.Run(stopCh)
+	speaker.NewController(config).Run(ctx.Done())
 }
