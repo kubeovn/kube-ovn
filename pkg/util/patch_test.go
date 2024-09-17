4b06bd07b2971f7deff8bed7465bf4bb28197f33
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2024-09-17 20:27:59 +0800

    add pod patch and exec ut (#4521)
    * add ut
    
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
    
    * add pod exec ut
    
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
    
    ---------
    
    Signed-off-by: bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>

--- pkg/util/patch_test.go
+++ pkg/util/patch_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -84,4 +85,86 @@ func TestGenerateStrategicMergePatchPayload(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGenerateMergePatchPayload(t *testing.T) {
+	type args struct {
+		// original stands for the original object we seen before we handle
+		original runtime.Object
+		// modified stands for the modified object
+		modified runtime.Object
+		// remote stands for the latest object in the server before apply patch
+		remote runtime.Object
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    v1.Pod
+		wantErr bool
+	}{
+		{
+			name: "base",
+			args: args{
+				original: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+				modified: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"ovn1": "1", "ovn2": "2"}}},
+				remote:   &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+			},
+			want:    v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"ovn1": "1", "ovn2": "2"}}},
+			wantErr: false,
+		},
+		{
+			name: "baseWithRemote",
+			args: args{
+				original: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+				modified: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"ovn1": "1", "ovn2": "2"}}},
+				remote:   &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"calico1": "1"}}},
+			},
+			want:    v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"calico1": "1", "ovn1": "1", "ovn2": "2"}}},
+			wantErr: false,
+		},
+		{
+			name: "baseWithoutAll",
+			args: args{
+				original: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+				modified: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+				remote:   &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+			},
+			want:    v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: nil}},
+			wantErr: false,
+		},
+		{
+			name: "baseWithoutModified",
+			args: args{
+				original: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"calico1": "1"}}},
+				modified: &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{}}},
+				remote:   &v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: map[string]string{"calico1": "1"}}},
+			},
+			want:    v1.Pod{ObjectMeta: metav1.ObjectMeta{Annotations: nil}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateMergePatchPayload(tt.args.original, tt.args.modified)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateMergePatchPayload() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			b, _ := json.Marshal(tt.args.remote)
+			// apply patch for remote obj
+			newB, _ := strategicpatch.StrategicMergePatch(b, got, v1.Pod{})
+			patchedPod := v1.Pod{}
+			_ = json.Unmarshal(newB, &patchedPod)
+			if !assert.Equal(t, tt.want, patchedPod, "patch: %s", got) {
+				t.Errorf("patch not correct, got = %+v, want= %+v", patchedPod, tt.want)
+			}
+		})
+	}
+}
+
+func TestFailedGenerateStrategicMergePatchPayload(t *testing.T) {
+	// test original and modified object are nil
+	got, err := GenerateStrategicMergePatchPayload(nil, nil)
+	require.Error(t, err)
+	require.Nil(t, got)
 }
