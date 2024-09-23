882596855922b2c272016683d8647a1a4b65b6d0
SkalaNetworks <contact@skala.network>
2024-09-23 12:03:00 -0400

    feat(natgw): make the prefix for nat gws configurable
    Signed-off-by: SkalaNetworks <contact@skala.network>

--- pkg/util/vpc_nat_gateway.go
+++ pkg/util/vpc_nat_gateway.go
@@ -2,10 +2,17 @@ package util
 
 import "fmt"
 
+var (
+	// VpcNatGwNamePrefix is appended to the name of the StatefulSet and Pods for NAT gateways
+	VpcNatGwNamePrefix = "vpc-nat-gw"
+)
+
+// GenNatGwStsName returns the full name of a NAT gateway StatefulSet
 func GenNatGwStsName(name string) string {
-	return fmt.Sprintf("vpc-nat-gw-%s", name)
+	return fmt.Sprintf("%s-%s", VpcNatGwNamePrefix, name)
 }
 
+// GenNatGwPodName returns the full name of the NAT gateway pod within a StatefulSet
 func GenNatGwPodName(name string) string {
-	return fmt.Sprintf("vpc-nat-gw-%s-0", name)
+	return fmt.Sprintf("%s-%s-0", VpcNatGwNamePrefix, name)
 }
