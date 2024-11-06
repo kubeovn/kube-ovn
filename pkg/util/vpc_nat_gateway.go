fd1792e8d8b674ca5c559b8e07ad50df0be91c6c
SKALA NETWORKS <127797154+SkalaNetworks@users.noreply.github.com>
2024-11-06 02:38:54 +0100

    Make NAT gateway generation prefix a variable (#4557)
    Signed-off-by: SkalaNetworks <contact@skala.network>

--- pkg/util/vpc_nat_gateway.go
+++ pkg/util/vpc_nat_gateway.go
@@ -2,10 +2,18 @@ package util
 
 import "fmt"
 
+// VpcNatGwNameDefaultPrefix is the default prefix appended to the name of the NAT gateways
+const VpcNatGwNameDefaultPrefix = "vpc-nat-gw"
+
+// VpcNatGwNamePrefix is appended to the name of the StatefulSet and Pods for NAT gateways
+var VpcNatGwNamePrefix = VpcNatGwNameDefaultPrefix
+
+// GenNatGwStsName returns the full name of a NAT gateway StatefulSet
 func GenNatGwStsName(name string) string {
-	return fmt.Sprintf("vpc-nat-gw-%s", name)
+	return fmt.Sprintf("%s-%s", VpcNatGwNamePrefix, name)
 }
 
+// GenNatGwPodName returns the full name of the NAT gateway pod within a StatefulSet
 func GenNatGwPodName(name string) string {
-	return fmt.Sprintf("vpc-nat-gw-%s-0", name)
+	return fmt.Sprintf("%s-%s-0", VpcNatGwNamePrefix, name)
 }
