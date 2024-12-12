0ae880e3b6d9d3b3bf39cc1dd9feae0add441453
zhangzujian <zhangzujian.7@gmail.com>
2024-12-12 08:38:33 +0000

    update unittests
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/controller/admin_network_policy_test.go
+++ pkg/controller/admin_network_policy_test.go
@@ -3,12 +3,13 @@ package controller
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/kubeovn/kube-ovn/pkg/util"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1alpha1 "sigs.k8s.io/network-policy-api/apis/v1alpha1"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/kubeovn/kube-ovn/pkg/ovsdb/ovnnb"
+	"github.com/kubeovn/kube-ovn/pkg/util"
 )
 
 func TestValidateAnpConfig(t *testing.T) {
@@ -22,22 +23,24 @@ func TestValidateAnpConfig(t *testing.T) {
 			Name: "anp-prio",
 		},
 		Spec: v1alpha1.AdminNetworkPolicySpec{
-			Priority: 100,
+			Priority: util.AnpMaxPriority,
 			Subject:  v1alpha1.AdminNetworkPolicySubject{Namespaces: &metav1.LabelSelector{}},
 		},
 	}
 
-	if ctrl.anpPrioNameMap == nil {
-		ctrl.anpPrioNameMap = make(map[int32]string, 1)
-	}
-	ctrl.anpPrioNameMap[anp.Spec.Priority] = anp.Name + "test"
-	t.Run("check same priority conflict", func(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		err := ctrl.validateAnpConfig(anp)
+		require.NoError(t, err)
+	})
+
+	t.Run("conflict priority", func(t *testing.T) {
+		ctrl.anpPrioNameMap = map[int32]string{anp.Spec.Priority: anp.Name + "-conflict"}
 		err := ctrl.validateAnpConfig(anp)
 		require.ErrorContains(t, err, "can not create anp with same priority")
 	})
 
-	delete(ctrl.anpPrioNameMap, anp.Spec.Priority)
-	t.Run("check priority out of range", func(t *testing.T) {
+	t.Run("priority out of range", func(t *testing.T) {
+		anp.Spec.Priority = util.AnpMaxPriority + 1
 		err := ctrl.validateAnpConfig(anp)
 		require.ErrorContains(t, err, "is greater than max value")
 	})
@@ -46,62 +49,179 @@ func TestValidateAnpConfig(t *testing.T) {
 func TestFetchCIDRAddrs(t *testing.T) {
 	t.Parallel()
 
-	networks := []v1alpha1.CIDR{"10.10.10.0/24", "fd00:10:96::/112"}
-	t.Run("check cidr address", func(t *testing.T) {
-		v4Addresses, v6Addresses := fetchCIDRAddrs(networks)
-		require.EqualValues(t, v4Addresses, []string{"10.10.10.0/24"})
-		require.EqualValues(t, v6Addresses, []string{"fd00:10:96::/112"})
-	})
+	tests := []struct {
+		name     string
+		networks []v1alpha1.CIDR
+		v4Addrs  []string
+		v6Addrs  []string
+	}{
+		{
+			name: "nil",
+		}, {
+			name:     "empty",
+			networks: []v1alpha1.CIDR{},
+		}, {
+			name:     "ipv4 only",
+			networks: []v1alpha1.CIDR{"1.1.1.0/24"},
+			v4Addrs:  []string{"1.1.1.0/24"},
+		},
+		{
+			name:     "ipv6 only",
+			networks: []v1alpha1.CIDR{"fd00::/64"},
+			v6Addrs:  []string{"fd00::/64"},
+		},
+		{
+			name:     "mixed",
+			networks: []v1alpha1.CIDR{"1.1.1.0/24", "fd00::/64"},
+			v4Addrs:  []string{"1.1.1.0/24"},
+			v6Addrs:  []string{"fd00::/64"},
+		},
+		{
+			name:     "invalid ipv4 cidr",
+			networks: []v1alpha1.CIDR{"1.1.1.0/33", "fd00::/64"},
+			v6Addrs:  []string{"fd00::/64"},
+		},
+		{
+			name:     "invalid ipv6 cidr",
+			networks: []v1alpha1.CIDR{"1.1.1.0/24", "fd00::/129"},
+			v4Addrs:  []string{"1.1.1.0/24"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v4Addrs, v6Addrs := fetchCIDRAddrs(tt.networks)
+			require.Equal(t, tt.v4Addrs, v4Addrs)
+			require.Equal(t, tt.v6Addrs, v6Addrs)
+		})
+	}
 }
 
 func TestIsRulesArrayEmpty(t *testing.T) {
 	t.Parallel()
 
-	ruleNames := [util.AnpMaxRules]ChangedName{{isMatch: true}}
-	t.Run("check rules array empty", func(t *testing.T) {
-		isEmpty := isRulesArrayEmpty(ruleNames)
-		require.False(t, isEmpty)
-	})
+	tests := []struct {
+		name string
+		arg  [util.AnpMaxRules]ChangedName
+		ret  bool
+	}{
+		{
+			"empty",
+			[util.AnpMaxRules]ChangedName{},
+			true,
+		},
+		{
+			"rule name",
+			[util.AnpMaxRules]ChangedName{{curRuleName: "foo"}},
+			false,
+		},
+		{
+			"match",
+			[util.AnpMaxRules]ChangedName{{isMatch: true}},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ret := isRulesArrayEmpty(tt.arg)
+			require.Equal(t, tt.ret, ret)
+		})
+	}
 }
 
 func TestGetAnpName(t *testing.T) {
 	t.Parallel()
 
-	anpName := "test"
-	t.Run("check anp name", func(t *testing.T) {
-		checkName := getAnpName(anpName)
-		require.EqualValues(t, checkName, anpName)
-	})
+	tests := []struct {
+		name string
+		arg  string
+		ret  string
+	}{
+		{
+			"normal",
+			"foo",
+			"foo",
+		},
+		{
+			"start with digital",
+			"123",
+			"anp123",
+		},
+		{
+			"start with hyphen",
+			"-foo",
+			"anp-foo",
+		},
+		{
+			"start with underscore",
+			"_foo",
+			"anp_foo",
+		},
+		{
+			"start with dot",
+			".foo",
+			"anp.foo",
+		},
+		{
+			"start with slash",
+			"/foo",
+			"anp/foo",
+		},
+		{
+			"start with colon",
+			":foo",
+			"anp:foo",
+		},
+	}
 
-	anpName = "123"
-	t.Run("check anp name start with digital", func(t *testing.T) {
-		checkName := getAnpName(anpName)
-		require.EqualValues(t, checkName, "anp"+anpName)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ret := getAnpName(tt.arg)
+			require.Equal(t, tt.ret, ret)
+		})
+	}
 }
 
 func TestGetAnpAddressSetName(t *testing.T) {
 	t.Parallel()
 
-	pgName := "portgroup"
-	ruleName := "rule"
-	index := 1
-	inAsV4Name := "portgroup.ingress.1.rule.IPv4"
-	inAsV6Name := "portgroup.ingress.1.rule.IPv6"
-	outAsV4Name := "portgroup.egress.1.rule.IPv4"
-	outAsV6Name := "portgroup.egress.1.rule.IPv6"
+	tests := []struct {
+		name      string
+		pgName    string
+		ruleName  string
+		index     int
+		isIngress bool
+		v4Name    string
+		v6Name    string
+	}{
+		{
+			"ingress",
+			"foo",
+			"bar",
+			1,
+			true,
+			"foo.ingress.1.bar.IPv4",
+			"foo.ingress.1.bar.IPv6",
+		},
+		{
+			"egress",
+			"bar",
+			"foo",
+			0,
+			false,
+			"bar.egress.0.foo.IPv4",
+			"bar.egress.0.foo.IPv6",
+		},
+	}
 
-	t.Run("check ingress address_set name", func(t *testing.T) {
-		asV4Name, asV6Name := getAnpAddressSetName(pgName, ruleName, index, true)
-		require.EqualValues(t, asV4Name, inAsV4Name)
-		require.EqualValues(t, asV6Name, inAsV6Name)
-	})
-
-	t.Run("check egress address_set name", func(t *testing.T) {
-		asV4Name, asV6Name := getAnpAddressSetName(pgName, ruleName, index, false)
-		require.EqualValues(t, asV4Name, outAsV4Name)
-		require.EqualValues(t, asV6Name, outAsV6Name)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v4Name, v6Name := getAnpAddressSetName(tt.pgName, tt.ruleName, tt.index, tt.isIngress)
+			require.Equal(t, tt.v4Name, v4Name)
+			require.Equal(t, tt.v6Name, v6Name)
+		})
+	}
 }
 
 func TestIsLabelsMatch(t *testing.T) {
@@ -141,4 +261,42 @@ func TestIsLabelsMatch(t *testing.T) {
 		isMatch = isLabelsMatch(nil, &nsPod, nsCmpLabelMap, podCmpLabelMap)
 		require.False(t, isMatch)
 	})
+}
+
+func TestAnpACLAction(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		arg  v1alpha1.AdminNetworkPolicyRuleAction
+		ret  ovnnb.ACLAction
+	}{
+		{
+			"allow",
+			v1alpha1.AdminNetworkPolicyRuleActionAllow,
+			ovnnb.ACLActionAllowRelated,
+		},
+		{
+			"pass",
+			v1alpha1.AdminNetworkPolicyRuleActionPass,
+			ovnnb.ACLActionPass,
+		},
+		{
+			"deny",
+			v1alpha1.AdminNetworkPolicyRuleActionDeny,
+			ovnnb.ACLActionDrop,
+		},
+		{
+			"unknown",
+			"foo",
+			ovnnb.ACLActionDrop,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ret := anpACLAction(tt.arg)
+			require.Equal(t, tt.ret, ret)
+		})
+	}
 }
