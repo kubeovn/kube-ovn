a0d895dbd6003e30ae7c26d89a275d483481dfa2
dolibali <zh98819@163.com>
2025-01-03 09:52:15 +0000

    Direct reference
    Signed-off-by: dolibali <zh98819@163.com>

--- cmd/daemon/init.go
+++ cmd/daemon/init.go
@@ -12,38 +12,33 @@ import (
 	"github.com/kubeovn/kube-ovn/pkg/util"
 )
 
-const (
-	geneveLinkName = util.GeneveNic
-	vxlanLinkName  = util.VxlanNic
-)
-
 func printCaps() {
 	currentCaps := cap.GetProc()
 	klog.Infof("current capabilities: %s", currentCaps.String())
 }
 
 func initForOS() error {
-	if _, err := netlink.LinkByName(geneveLinkName); err != nil {
+	if _, err := netlink.LinkByName(util.GeneveNic); err != nil {
 		if _, ok := err.(netlink.LinkNotFoundError); ok {
 			return nil
 		}
-		klog.Errorf("failed to get link %s: %v", geneveLinkName, err)
+		klog.Errorf("failed to get link %s: %v", util.GeneveNic, err)
 		return err
 	}
 
 	// disable checksum for genev_sys_6081 as default
-	return daemon.TurnOffNicTxChecksum(geneveLinkName)
+	return daemon.TurnOffNicTxChecksum(util.GeneveNic)
 }
 
 func setVxlanNicTxOff() error {
-	if _, err := netlink.LinkByName(vxlanLinkName); err != nil {
+	if _, err := netlink.LinkByName(util.VxlanNic); err != nil {
 		if _, ok := err.(netlink.LinkNotFoundError); ok {
 			return nil
 		}
-		klog.Errorf("failed to get link %s: %v", vxlanLinkName, err)
+		klog.Errorf("failed to get link %s: %v", util.VxlanNic, err)
 		return err
 	}
 
 	// disable checksum for vxlan_sys_4789 as default
-	return daemon.TurnOffNicTxChecksum(vxlanLinkName)
+	return daemon.TurnOffNicTxChecksum(util.VxlanNic)
 }
