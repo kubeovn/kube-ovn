9abcb24bb5b90e8bae9d8800fd1027e1798b94d4
zhangzujian <zhangzujian.7@gmail.com>
2025-02-10 03:43:55 +0000

    fix superfluous response.WriteHeader
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/util/health_check.go
+++ pkg/util/health_check.go
@@ -8,6 +8,6 @@ import (
 
 func DefaultHealthCheckHandler(w http.ResponseWriter, _ *http.Request) {
 	if _, err := w.Write([]byte("ok")); err != nil {
-		klog.Error(err)
+		klog.Errorf("failed to write health check response: %v", err)
 	}
 }
