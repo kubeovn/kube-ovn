d030323b17070892ec2b690c07ecce12535e888b
zhangzujian <zhangzujian.7@gmail.com>
2025-02-13 01:42:44 +0000

    bump go to 1.24.0
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- hack/update-codegen-docker.sh
+++ hack/update-codegen-docker.sh
@@ -7,7 +7,7 @@ GOPROXY=${GOPROXY:-"https://goproxy.cn"}
 docker run -it --rm \
     -v ${PWD}:/app \
     -e GOPROXY=${GOPROXY} \
-    ghcr.io/zhangzujian/kube-code-generator:v0.5.1 \
+    ghcr.io/zhangzujian/kube-code-generator:v0.5.2 \
     --boilerplate-path ./hack/boilerplate.go.txt \
     --apis-in ./pkg/apis \
     --go-gen-out ./pkg/client
