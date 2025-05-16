3780abbd047c70ea4887682da02f5e97f41dd22f
zhangzujian <zhangzujian.7@gmail.com>
2025-05-16 01:15:42 +0000

    bump k8s to v1.33.0
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/client/informers/externalversions/kubeovn/v1/ippool.go
+++ pkg/client/informers/externalversions/kubeovn/v1/ippool.go
@@ -61,13 +61,25 @@ func NewFilteredIPPoolInformer(client versioned.Interface, resyncPeriod time.Dur
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubeovnV1().IPPools().List(context.TODO(), options)
+				return client.KubeovnV1().IPPools().List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubeovnV1().IPPools().Watch(context.TODO(), options)
+				return client.KubeovnV1().IPPools().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubeovnV1().IPPools().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubeovnV1().IPPools().Watch(ctx, options)
 			},
 		},
 		&apiskubeovnv1.IPPool{},
