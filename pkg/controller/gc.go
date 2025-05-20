224d30cc6ba5b787d2592762f89c6c716e854b39
SKALA NETWORKS <127797154+SkalaNetworks@users.noreply.github.com>
2025-05-21 01:56:27 +0200

    chore(vpc-nat-gw): refactor before adding HA gw (#5197)
    Signed-off-by: SkalaNetworks <contact@skala.network>

--- pkg/controller/gc.go
+++ pkg/controller/gc.go
@@ -102,7 +102,7 @@ func (c *Controller) gcVpcNatGateway() error {
 				return err
 			}
 		}
-		gwStsNames = append(gwStsNames, util.GenNatGwStsName(gw.Name))
+		gwStsNames = append(gwStsNames, util.GenNatGwName(gw.Name))
 	}
 
 	sel, _ := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{MatchLabels: map[string]string{util.VpcNatGatewayLabel: "true"}})
