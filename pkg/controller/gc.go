94c514e73bd688299124918b523c5a0d1549ad95
SKALA NETWORKS <127797154+SkalaNetworks@users.noreply.github.com>
2025-06-23 10:35:52 +0200

    fix(slr): support switchlbrules targeting subnets with non-default providers (#5376)
    Signed-off-by: SkalaNetworks <contact@skala.network>

--- pkg/controller/gc.go
+++ pkg/controller/gc.go
@@ -395,7 +395,19 @@ func (c *Controller) markAndCleanLSP() error {
 	vipsMap := strset.NewWithSize(len(vips))
 	for _, vip := range vips {
 		if vip.Spec.Type != "" {
-			portName := ovs.PodNameToPortName(vip.Name, vip.Spec.Namespace, util.OvnProvider)
+			subnetName := vip.Spec.Subnet
+			if subnetName == "" {
+				klog.Errorf("failed to gc vip '%s', subnet should be set", vip.Name)
+				continue
+			}
+
+			subnet, err := c.subnetsLister.Get(subnetName)
+			if err != nil {
+				klog.Errorf("failed to get subnet %s: %v", subnetName, err)
+				continue
+			}
+
+			portName := ovs.PodNameToPortName(vip.Name, vip.Spec.Namespace, subnet.Spec.Provider)
 			vipsMap.Add(portName)
 		}
 	}
