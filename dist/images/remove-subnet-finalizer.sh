f39ff5490ae1773fdde33cf9240314fdf889ba93
Mengxin Liu <liumengxinfly@gmail.com>
2025-07-21 09:29:23 +0000

    fix: ip/vpc are not deleted by helm uninstall
    Signed-off-by: Mengxin Liu <liumengxinfly@gmail.com>

--- dist/images/remove-subnet-finalizer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-for subnet in $(kubectl get subnet -o name); do
-  kubectl patch "$subnet" --type='json' -p '[{"op": "replace", "path": "/metadata/finalizers", "value": []}]'
-done
