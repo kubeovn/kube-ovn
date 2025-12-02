188fa13b1596eb3b10fe86a0deeb2ec74cd89261
zhangzujian <zhangzujian.7@gmail.com>
2025-12-02 01:34:52 +0000

    fix windows
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/ovs/ovs-appctl_windows.go
+++ pkg/ovs/ovs-appctl_windows.go
@@ -1,18 +1,13 @@
 package ovs
 
-import "fmt"
+import "errors"
 
 const (
-	ovsRunDir = "/var/run/openvswitch"
-	ovnRunDir = "/var/run/ovn"
-
-	cmdOvsAppctl = "ovs-appctl"
-
 	OvsdbServer   = "ovsdb-server"
 	OvsVswitchd   = "ovs-vswitchd"
 	OvnController = "ovn-controller"
 )
 
 func Appctl(_ string, _ ...string) (string, error) {
-	return "", fmt.Errorf("Appctl is not implemented on Windows")
+	return "", errors.New("ovs-appctl is not implemented on Windows")
 }
