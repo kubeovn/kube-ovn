335d77d1d7d31630b6e6b0501dac4d046a532e4b
zhangzujian <zhangzujian.7@gmail.com>
2025-12-27 03:09:01 +0000

    update
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -68,19 +68,17 @@ func newFakeControllerWithOptions(t *testing.T, opts *FakeControllerOptions) (*f
 		opts = &FakeControllerOptions{}
 	}
 
-	// Create default namespace if none provided
-	defaultNamespace := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "default",
-			Annotations: map[string]string{
-				util.LogicalSwitchAnnotation: util.DefaultSubnet,
-			},
-		},
-	}
-
 	namespaces := opts.Namespaces
 	if len(namespaces) == 0 {
-		namespaces = []*corev1.Namespace{defaultNamespace}
+		// Create default namespace if none provided
+		namespaces = []*corev1.Namespace{{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "default",
+				Annotations: map[string]string{
+					util.LogicalSwitchAnnotation: util.DefaultSubnet,
+				},
+			},
+		}}
 	}
 
 	// Create fake Kubernetes client with namespaces and pods
@@ -104,30 +102,45 @@ func newFakeControllerWithOptions(t *testing.T, opts *FakeControllerOptions) (*f
 	}
 
 	// Create fake KubeOVN client
-	// subnetObjects := make([]runtime.Object, 0, len(opts.Subnets))
-	// for _, subnet := range opts.Subnets {
-	// 	subnetObjects = append(subnetObjects, subnet)
-	// }
-	// kubeovnClient := kubeovnfake.NewClientset(subnetObjects...)
-	kubeovnClient := kubeovnfake.NewClientset()
+	subnetObjects := make([]runtime.Object, 0, len(opts.Subnets))
+	for _, subnet := range opts.Subnets {
+		subnetObjects = append(subnetObjects, subnet)
+	}
+	kubeovnClient := kubeovnfake.NewClientset(subnetObjects...)
 	for _, subnet := range opts.Subnets {
 		_, err := kubeovnClient.KubeovnV1().Subnets().Create(
 			context.Background(), subnet, metav1.CreateOptions{})
 		if err != nil {
+			t.Errorf("failed to create fake subnet %s: %v", subnet.Name, err)
 			return nil, err
 		}
 	}
 
 	// Create informer factories
-	kubeInformerFactory := informers.NewSharedInformerFactory(kubeClient, 0)
+	kubeInformerFactory := informers.NewSharedInformerFactoryWithOptions(kubeClient, 0, informers.WithTweakListOptions(
+		func(options *metav1.ListOptions) {
+			options.Watch = true
+			options.AllowWatchBookmarks = true
+		},
+	))
 	serviceInformer := kubeInformerFactory.Core().V1().Services()
 	namespaceInformer := kubeInformerFactory.Core().V1().Namespaces()
 	podInformer := kubeInformerFactory.Core().V1().Pods()
 
-	nadInformerFactory := nadinformers.NewSharedInformerFactory(nadClient, 0)
+	nadInformerFactory := nadinformers.NewSharedInformerFactoryWithOptions(nadClient, 0, nadinformers.WithTweakListOptions(
+		func(options *metav1.ListOptions) {
+			options.Watch = true
+			options.AllowWatchBookmarks = true
+		},
+	))
 	nadInformer := nadInformerFactory.K8sCniCncfIo().V1().NetworkAttachmentDefinitions()
 
-	kubeovnInformerFactory := kubeovninformerfactory.NewSharedInformerFactory(kubeovnClient, 0)
+	kubeovnInformerFactory := kubeovninformerfactory.NewSharedInformerFactoryWithOptions(kubeovnClient, 0, kubeovninformerfactory.WithTweakListOptions(
+		func(options *metav1.ListOptions) {
+			options.Watch = true
+			options.AllowWatchBookmarks = true
+		},
+	))
 	vpcInformer := kubeovnInformerFactory.Kubeovn().V1().Vpcs()
 	subnetInformer := kubeovnInformerFactory.Kubeovn().V1().Subnets()
 	vpcNatGwInformer := kubeovnInformerFactory.Kubeovn().V1().VpcNatGateways()
@@ -202,22 +215,13 @@ func newFakeController(t *testing.T) *fakeController {
 }
 
 func Test_allSubnetReady(t *testing.T) {
-	// fakeController, err := newFakeControllerWithOptions(t, &FakeControllerOptions{
-	// 	Subnets: []*kubeovnv1.Subnet{{
-	// 		TypeMeta: metav1.TypeMeta{
-	// 			Kind:       reflect.TypeFor[kubeovnv1.Subnet]().Name(),
-	// 			APIVersion: kubeovn.GroupName,
-	// 		},
-	// 		ObjectMeta: metav1.ObjectMeta{Name: util.DefaultSubnet},
-	// 	}, {
-	// 		TypeMeta: metav1.TypeMeta{
-	// 			Kind:       reflect.TypeFor[kubeovnv1.Subnet]().Name(),
-	// 			APIVersion: kubeovn.GroupName,
-	// 		},
-	// 		ObjectMeta: metav1.ObjectMeta{Name: "join"},
-	// 	}},
-	// })
-	fakeController, err := newFakeControllerWithOptions(t, nil)
+	fakeController, err := newFakeControllerWithOptions(t, &FakeControllerOptions{
+		Subnets: []*kubeovnv1.Subnet{{
+			ObjectMeta: metav1.ObjectMeta{Name: util.DefaultSubnet},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{Name: "join"},
+		}},
+	})
 	require.NoError(t, err)
 	ctrl := fakeController.fakeController
 	mockOvnClient := fakeController.mockOvnClient
@@ -252,8 +256,12 @@ func Test_allSubnetReady(t *testing.T) {
 // TestFakeControllerWithOptions demonstrates usage of the unified fake controller
 func TestFakeControllerWithOptions(t *testing.T) {
 	// Example: creating a fake controller with NADs, subnets, and pods
-	nads := []*nadv1.NetworkAttachmentDefinition{
-		{
+	opts := &FakeControllerOptions{
+		Subnets: []*kubeovnv1.Subnet{{
+			ObjectMeta: metav1.ObjectMeta{Name: "net1-subnet"},
+			Spec:       kubeovnv1.SubnetSpec{CIDRBlock: "192.168.1.0/24"},
+		}},
+		NetworkAttachments: []*nadv1.NetworkAttachmentDefinition{{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "net1",
 				Namespace: "default",
@@ -261,18 +269,8 @@ func TestFakeControllerWithOptions(t *testing.T) {
 			Spec: nadv1.NetworkAttachmentDefinitionSpec{
 				Config: `{"cniVersion": "0.3.1", "name": "net1", "type": "kube-ovn"}`,
 			},
-		},
-	}
-
-	subnets := []*kubeovnv1.Subnet{
-		{
-			ObjectMeta: metav1.ObjectMeta{Name: "net1-subnet"},
-			Spec:       kubeovnv1.SubnetSpec{CIDRBlock: "192.168.1.0/24"},
-		},
-	}
-
-	pods := []*corev1.Pod{
-		{
+		}},
+		Pods: []*corev1.Pod{{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "test-pod",
 				Namespace: "default",
@@ -280,13 +278,7 @@ func TestFakeControllerWithOptions(t *testing.T) {
 					nadv1.NetworkAttachmentAnnot: `[{"name": "net1"}]`,
 				},
 			},
-		},
-	}
-
-	opts := &FakeControllerOptions{
-		Subnets:            subnets,
-		NetworkAttachments: nads,
-		Pods:               pods,
+		}},
 	}
 
 	fakeCtrl, err := newFakeControllerWithOptions(t, opts)
