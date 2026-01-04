78b6de652fcd4dec3ad330adbbacfe236661175a
changluyi <47097611+changluyi@users.noreply.github.com>
2026-01-04 13:18:34 +0800

    Add open flow sync refer to ovn-k8s (#6117)
    * add openflow sync refer to ovn-k8s
    
    Signed-off-by: clyi <clyi@alauda.io>

--- pkg/ovs/ovs-ofctl.go
+++ pkg/ovs/ovs-ofctl.go
@@ -2,145 +2,50 @@ package ovs
 
 import (
 	"fmt"
-	"net"
+	"os/exec"
 	"slices"
+	"strings"
 
 	"github.com/digitalocean/go-openvswitch/ovs"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 
-	kubeovnv1 "github.com/kubeovn/kube-ovn/pkg/apis/kubeovn/v1"
 	"github.com/kubeovn/kube-ovn/pkg/util"
 )
 
-func AddOrUpdateUnderlaySubnetSvcLocalOpenFlow(client *ovs.Client, bridgeName, lbServiceIP, protocol, dstMAC, underlayNic string, lbServicePort uint16) error {
-	isIPv6 := util.CheckProtocol(lbServiceIP) == kubeovnv1.ProtocolIPv6
-	var inPortID, outPortID int
-	var lrpMacAddr net.HardwareAddr
-	var err error
-	var cookie uint64
-
-	portInfo, err := client.OpenFlow.DumpPort(bridgeName, underlayNic)
+func DumpFlows(client *ovs.Client, bridgeName string) ([]string, error) {
+	flows, err := client.OpenFlow.DumpFlows(bridgeName)
 	if err != nil {
-		klog.Errorf("failed to dump bridge %s port %s: %v", bridgeName, underlayNic, err)
-		return err
-	}
-	inPortID = portInfo.PortID
-	klog.V(3).Infof("underlayNic %s's portID is %d", underlayNic, inPortID)
-
-	portInfo, err = client.OpenFlow.DumpPort(bridgeName, "patch-localnet.")
-	if err != nil {
-		klog.Errorf("failed to dump bridge %s port %s: %v", bridgeName, "patch-localnet.", err)
-		return err
-	}
-	outPortID = portInfo.PortID
-
-	lrpMacAddr, err = net.ParseMAC(dstMAC)
-	if err != nil {
-		klog.Errorf("failed to parse MAC address %s: %v", dstMAC, err)
-		return err
+		return nil, fmt.Errorf("ovs-ofctl dump-flows failed: %w", err)
 	}
 
-	cookie = util.UnderlaySvcLocalOpenFlowCookieV4
-	if isIPv6 {
-		cookie = util.UnderlaySvcLocalOpenFlowCookieV6
-	}
-
-	var protocolType ovs.Protocol
-	switch protocol {
-	case string(corev1.ProtocolTCP):
-		protocolType = ovs.ProtocolTCPv4
-		if isIPv6 {
-			protocolType = ovs.ProtocolTCPv6
+	flowStrings := make([]string, 0, len(flows))
+	for _, flow := range flows {
+		text, err := flow.MarshalText()
+		if err != nil {
+			return nil, fmt.Errorf("marshal flow failed: %w", err)
 		}
-	case string(corev1.ProtocolUDP):
-		protocolType = ovs.ProtocolUDPv4
-		if isIPv6 {
-			protocolType = ovs.ProtocolUDPv6
+		line := strings.TrimSpace(string(text))
+		if line == "" {
+			continue
 		}
-	default:
-		return fmt.Errorf("unsupported protocol %s", protocol)
+		flowStrings = append(flowStrings, line)
 	}
 
-	flow := &ovs.Flow{
-		Priority: util.UnderlaySvcLocalOpenFlowPriority,
-		Protocol: protocolType,
-		InPort:   inPortID,
-		Actions:  []ovs.Action{ovs.ModDataLinkDestination(lrpMacAddr), ovs.Output(outPortID)},
-		Matches: []ovs.Match{
-			ovs.NetworkDestination(lbServiceIP),
-			ovs.TransportDestinationMaskedPort(lbServicePort, 0xffff),
-		},
-		Cookie: cookie,
-	}
-
-	klog.Infof("add bridge %s svc local policy openflow rule", bridgeName)
-	err = client.OpenFlow.AddFlow(bridgeName, flow)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return flowStrings, nil
 }
 
-func DeleteUnderlaySubnetSvcLocalOpenFlow(client *ovs.Client, bridgeName, lbServiceIP, protocol, underlayNic string, lbServicePort uint16) error {
-	isIPv6 := util.CheckProtocol(lbServiceIP) == kubeovnv1.ProtocolIPv6
-	var inPortID int
-	var cookie uint64
+// ReplaceFlows uses ovs-ofctl replace-flows because go-openvswitch does not provide a native API.
+func ReplaceFlows(bridgeName string, flows []string) error {
+	flowData := strings.Join(flows, "\n")
 
-	cookie = util.UnderlaySvcLocalOpenFlowCookieV4
-	if isIPv6 {
-		cookie = util.UnderlaySvcLocalOpenFlowCookieV6
-	}
+	cmd := exec.Command("ovs-ofctl", "--bundle", "replace-flows", bridgeName, "-")
+	cmd.Stdin = strings.NewReader(flowData)
 
-	var protocolType ovs.Protocol
-	switch protocol {
-	case string(corev1.ProtocolTCP):
-		protocolType = ovs.ProtocolTCPv4
-		if isIPv6 {
-			protocolType = ovs.ProtocolTCPv6
-		}
-	case string(corev1.ProtocolUDP):
-		protocolType = ovs.ProtocolUDPv4
-		if isIPv6 {
-			protocolType = ovs.ProtocolUDPv6
-		}
-	default:
-		return fmt.Errorf("unsupported protocol %s", protocol)
-	}
-
-	portInfo, err := client.OpenFlow.DumpPort(bridgeName, underlayNic)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		klog.Errorf("failed to dump bridge %s port %s: %v", bridgeName, underlayNic, err)
-		return err
-	}
-	inPortID = portInfo.PortID
-	klog.V(3).Infof("underlayNic %s's portID is %d", underlayNic, inPortID)
-
-	match := &ovs.MatchFlow{
-		Protocol: protocolType,
-		InPort:   inPortID,
-		Matches: []ovs.Match{
-			ovs.NetworkDestination(lbServiceIP),
-			ovs.TransportDestinationMaskedPort(lbServicePort, 0xffff),
-		},
-		Cookie: cookie,
+		return fmt.Errorf("ovs-ofctl replace-flows failed: %w, output: %s", err, strings.TrimSpace(string(output)))
 	}
 
-	oldflows, err := client.OpenFlow.DumpFlowsWithFlowArgs(bridgeName, match)
-	if err != nil {
-		klog.Errorf("failed to dump flows: %v", err)
-		return err
-	}
-
-	if len(oldflows) > 0 {
-		klog.Infof("remove bridge %s old svc local policy openflow rule", bridgeName)
-		err = client.OpenFlow.DelFlows(bridgeName, match)
-		if err != nil {
-			klog.Errorf("failed to remove old svc local policy openflow rule: %v", err)
-			return err
-		}
-	}
 	return nil
 }
 
