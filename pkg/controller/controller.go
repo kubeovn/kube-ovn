af72723b2e4f3d58c405b07443bc8537047ceac7
zbb88888 <jmdxjsjgcxy@gmail.com>
2026-01-26 14:19:45 +0800

    fix: caching NAD CRD should before all kubeovn crds and pod (#6198)
    * fix: caching NAD CRD should before all kubeovn crds and pod
    
    ---------
    
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -695,6 +695,10 @@ func Run(ctx context.Context, config *Configuration) {
 	defer controller.shutdown()
 	klog.Info("Starting OVN controller")
 
+	// Start and sync NAD informer first, as many resources depend on NAD cache
+	// NAD CRD is optional, so we check if it exists before starting the informer
+	controller.StartNetAttachInformerFactory(ctx)
+
 	// Wait for the caches to be synced before starting workers
 	controller.informerFactory.Start(ctx.Done())
 	controller.cmInformerFactory.Start(ctx.Done())
@@ -702,7 +706,6 @@ func Run(ctx context.Context, config *Configuration) {
 	controller.kubeovnInformerFactory.Start(ctx.Done())
 	controller.anpInformerFactory.Start(ctx.Done())
 	controller.StartKubevirtInformerFactory(ctx, kubevirtInformerFactory)
-	controller.StartNetAttachInformerFactory(ctx)
 
 	klog.Info("Waiting for informer caches to sync")
 	cacheSyncs := []cache.InformerSynced{
