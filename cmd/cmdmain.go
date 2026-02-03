5b5443530ab75564c35e9d4e06e7bd863b78b16f
Mengxin Liu <liumengxinfly@gmail.com>
2026-02-03 15:02:45 +0000

    refactor: profiling, Retry, assignAddress, runIfLinkExists
    - Add pkg/util/profiling with DumpProfile(); use from cmd/cmdmain.go and cmd/controller/cmdmain.go
    - cmd/cmdmain.go: remove local dumpProfile, use profiling.DumpProfile()
    - cmd/controller/cmdmain.go: remove duplicated dumpProfile, use profiling.DumpProfile()
    - cmd/daemon/cniserver.go: Retry param ctrl->cfg, return nil on success
    - cmd/daemon/init.go: add runIfLinkExists helper for initForOS and setVxlanNicTxOff
    - cmd/cni/cni.go: assignAddress(ipAddress, gateway, protocol, mask), merge IPv4/IPv6 case in generateCNIResult
    
    Signed-off-by: Mengxin Liu <liumengxinfly@gmail.com>

--- cmd/cmdmain.go
+++ cmd/cmdmain.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"runtime/pprof"
-	"syscall"
-	"time"
-
-	"k8s.io/klog/v2"
 
 	"github.com/kubeovn/kube-ovn/cmd/ovn_ic_controller"
 	"github.com/kubeovn/kube-ovn/cmd/ovn_leader_checker"
@@ -17,6 +10,7 @@ import (
 	"github.com/kubeovn/kube-ovn/cmd/speaker"
 	"github.com/kubeovn/kube-ovn/cmd/webhook"
 	"github.com/kubeovn/kube-ovn/pkg/util"
+	"github.com/kubeovn/kube-ovn/pkg/util/profiling"
 )
 
 const (
@@ -27,71 +21,14 @@ const (
 	CmdOvnICController  = "kube-ovn-ic-controller"
 )
 
-const timeFormat = "2006-01-02_15:04:05"
-
-func dumpProfile() {
-	ch1 := make(chan os.Signal, 1)
-	ch2 := make(chan os.Signal, 1)
-	signal.Notify(ch1, syscall.SIGUSR1)
-	signal.Notify(ch2, syscall.SIGUSR2)
-	go func() {
-		for {
-			<-ch1
-			name := fmt.Sprintf("cpu-profile-%s.pprof", time.Now().Format(timeFormat))
-			path := filepath.Join(os.TempDir(), name)
-			f, err := os.Create(path) // #nosec G303,G304
-			if err != nil {
-				klog.Errorf("failed to create cpu profile file: %v", err)
-				return
-			}
-			if err = pprof.StartCPUProfile(f); err != nil {
-				klog.Errorf("failed to start cpu profile: %v", err)
-				if err = f.Close(); err != nil {
-					klog.Errorf("failed to close file %q: %v", path, err)
-				}
-				return
-			}
-			time.Sleep(30 * time.Second)
-			pprof.StopCPUProfile()
-			if err = f.Close(); err != nil {
-				klog.Errorf("failed to close file %q: %v", path, err)
-				return
-			}
-		}
-	}()
-	go func() {
-		for {
-			<-ch2
-			name := fmt.Sprintf("mem-profile-%s.pprof", time.Now().Format(timeFormat))
-			path := filepath.Join(os.TempDir(), name)
-			f, err := os.Create(path) // #nosec G303,G304
-			if err != nil {
-				klog.Errorf("failed to create memory profile file: %v", err)
-				return
-			}
-			if err = pprof.WriteHeapProfile(f); err != nil {
-				klog.Errorf("failed to write memory profile file: %v", err)
-				if err = f.Close(); err != nil {
-					klog.Errorf("failed to close file %q: %v", path, err)
-				}
-				return
-			}
-			if err = f.Close(); err != nil {
-				klog.Errorf("failed to close file %q: %v", path, err)
-				return
-			}
-		}
-	}()
-}
-
 func main() {
 	cmd := filepath.Base(os.Args[0])
 	switch cmd {
 	case CmdMonitor:
-		dumpProfile()
+		profiling.DumpProfile()
 		ovn_monitor.CmdMain()
 	case CmdSpeaker:
-		dumpProfile()
+		profiling.DumpProfile()
 		speaker.CmdMain()
 	case CmdWebhook:
 		webhook.CmdMain()
