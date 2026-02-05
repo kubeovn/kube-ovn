926bfec9bafd68fb7e3c62485612ccc780b41dc2
张祖建 <zhangzujian.7@gmail.com>
2026-02-05 09:38:45 +0800

    security: specify a storage limit for containers (#6259)
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- dist/images/install.sh
+++ dist/images/install.sh
@@ -4736,6 +4736,7 @@ spec:
             limits:
               cpu: 4
               memory: 4Gi
+              ephemeral-storage: 1Gi
           volumeMounts:
             - mountPath: /var/run/ovn
               name: host-run-ovn
@@ -4957,6 +4958,7 @@ spec:
             limits:
               cpu: "2"
               memory: 1000Mi
+              ephemeral-storage: 1Gi
       nodeSelector:
         kubernetes.io/os: "linux"
       volumes:
@@ -5113,6 +5115,7 @@ spec:
               cpu: 1000m
               hugepages-2Mi: 1Gi
               memory: 800Mi
+              ephemeral-storage: 1Gi
       nodeSelector:
         kubernetes.io/os: "linux"
         ovn.kubernetes.io/ovs_dp_type: "userspace"
@@ -5376,6 +5379,7 @@ spec:
             limits:
               cpu: 1000m
               memory: 1Gi
+              ephemeral-storage: 1Gi
       nodeSelector:
         kubernetes.io/os: "linux"
         kube-ovn/role: master
@@ -5607,6 +5611,7 @@ spec:
           limits:
             cpu: 1000m
             memory: 1Gi
+            ephemeral-storage: 1Gi
       nodeSelector:
         kubernetes.io/os: "linux"
       volumes:
@@ -5774,6 +5779,7 @@ spec:
             limits:
               cpu: 200m
               memory: 200Mi
+              ephemeral-storage: 1Gi
           volumeMounts:
             - mountPath: /var/run/ovn
               name: host-run-ovn
@@ -5967,6 +5973,7 @@ spec:
             limits:
               cpu: 3
               memory: 1Gi
+              ephemeral-storage: 1Gi
           volumeMounts:
             - mountPath: /var/run/ovn
               name: host-run-ovn
@@ -6172,6 +6179,7 @@ spec:
             limits:
               cpu: 200m
               memory: 400Mi
+              ephemeral-storage: 1Gi
           livenessProbe:
             httpGet:
               path: /metrics
