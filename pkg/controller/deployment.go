2536c80dd0f8a2f99940130b2858c97ea0580420
SkalaNetworks <contact@skala.network>
2026-04-23 12:15:44 +0000

    feat(vpcnatgateway): improvements on reconciliation
    Signed-off-by: SkalaNetworks <contact@skala.network>

--- pkg/controller/deployment.go
+++ pkg/controller/deployment.go
@@ -11,12 +11,20 @@ import (
 
 func (c *Controller) enqueueAddDeployment(obj any) {
 	deploy := obj.(*appsv1.Deployment)
+	klog.Infof("enqueue update blabla")
 	for _, ref := range deploy.OwnerReferences {
-		if ref.APIVersion == kubeovnv1.SchemeGroupVersion.String() && ref.Kind == util.KindVpcEgressGateway {
-			key := types.NamespacedName{Namespace: deploy.Namespace, Name: ref.Name}.String()
-			klog.V(3).Infof("enqueue update vpc-egress-gateway %s", key)
-			c.addOrUpdateVpcEgressGatewayQueue.Add(key)
-			return
+		if ref.APIVersion == kubeovnv1.SchemeGroupVersion.String() {
+			if ref.Kind == util.KindVpcEgressGateway {
+				key := types.NamespacedName{Namespace: deploy.Namespace, Name: ref.Name}.String()
+				klog.V(3).Infof("enqueue update vpc-egress-gateway %s", key)
+				c.addOrUpdateVpcEgressGatewayQueue.Add(key)
+				return
+			}
+			if ref.Kind == util.KindVpcNatGateway {
+				klog.V(3).Infof("enqueue update vpc-nat-gw %s", ref.Name)
+				c.addOrUpdateVpcNatGatewayQueue.Add(ref.Name)
+				return
+			}
 		}
 	}
 }
