90abb973df1b248fb71619925df3bc1d3088d45e
Rohan <Rohan.Meshram@Kluisz.ai>
2026-06-01 06:22:51 +0530

    fix(controller): populate v6ip from cached EIP in handleAddVpcExternalSubnet (#6799)
    When the OvnEip already exists (controller restart/requeue), the else
    branch only read V4Ip and MacAddress but not V6Ip. This caused
    GetIPAddrWithMask to fail with 'ip should be dualstack' when the
    external subnet is dualstack, as it received only the v4 address
    against a dualstack CIDR.
    
    Signed-off-by: rohan-kluisz <Rohan.Meshram@Kluisz.ai>
    (cherry picked from commit 8bd070c1c6d4df79495223a6e3ae84b0b25e2e02)

--- pkg/controller/vpc.go
+++ pkg/controller/vpc.go
@@ -1346,9 +1346,10 @@ func (c *Controller) handleAddVpcExternalSubnet(key, subnet string) error {
 		}
 	} else {
 		v4ip = cachedEip.Spec.V4Ip
+		v6ip = cachedEip.Spec.V6Ip
 		mac = cachedEip.Spec.MacAddress
 	}
-	if v4ip == "" || mac == "" {
+	if (v4ip == "" && v6ip == "") || mac == "" {
 		err := fmt.Errorf("lrp '%s' ip or mac should not be empty", lrpEipName)
 		klog.Error(err)
 		return err
@@ -1382,7 +1383,7 @@ func (c *Controller) handleAddVpcExternalSubnet(key, subnet string) error {
 		return err
 	}
 
-	v4ipCidr, err := util.GetIPAddrWithMask(v4ip, cachedSubnet.Spec.CIDRBlock)
+	ipCidr, err := util.GetIPAddrWithMask(util.GetStringIP(v4ip, v6ip), cachedSubnet.Spec.CIDRBlock)
 	if err != nil {
 		klog.Error(err)
 		return err
@@ -1390,7 +1391,7 @@ func (c *Controller) handleAddVpcExternalSubnet(key, subnet string) error {
 	lspName := fmt.Sprintf("%s-%s", subnet, key)
 	lrpName := fmt.Sprintf("%s-%s", key, subnet)
 
-	if err := c.OVNNbClient.CreateLogicalPatchPort(subnet, key, lspName, lrpName, v4ipCidr, mac, chassises...); err != nil {
+	if err := c.OVNNbClient.CreateLogicalPatchPort(subnet, key, lspName, lrpName, ipCidr, mac, chassises...); err != nil {
 		klog.Errorf("failed to connect router '%s' to external: %v", key, err)
 		return err
 	}
