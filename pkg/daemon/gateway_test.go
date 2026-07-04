aae8da81e4e36ab8916a008141db6c06d07ebb04
张祖建 <zhangzujian.7@gmail.com>
2026-07-04 10:56:46 +0800

    fix: require subnet for attachment provider (#6965)
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/daemon/gateway_test.go
+++ pkg/daemon/gateway_test.go
@@ -1,12 +1,28 @@
 package daemon
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 
 	kubeovnv1 "github.com/kubeovn/kube-ovn/pkg/apis/kubeovn/v1"
+	kubeovnfake "github.com/kubeovn/kube-ovn/pkg/client/clientset/versioned/fake"
+	kubeovninformerfactory "github.com/kubeovn/kube-ovn/pkg/client/informers/externalversions"
+	"github.com/kubeovn/kube-ovn/pkg/util"
 )
+
+type errSubnetLister struct{}
+
+func (errSubnetLister) List(labels.Selector) ([]*kubeovnv1.Subnet, error) {
+	return nil, errors.New("list failed")
+}
+
+func (errSubnetLister) Get(string) (*kubeovnv1.Subnet, error) {
+	return nil, errors.New("get failed")
+}
 
 func TestGetCidrByProtocol(t *testing.T) {
 	cases := []struct {
@@ -58,4 +74,42 @@ func TestGetCidrByProtocol(t *testing.T) {
 			require.Equal(t, c.expected, got)
 		})
 	}
+}
+
+func TestProviderExistsRequiresSubnetForNamedOvnProvider(t *testing.T) {
+	subnets := []*kubeovnv1.Subnet{{
+		ObjectMeta: metav1.ObjectMeta{Name: util.DefaultSubnet},
+		Spec:       kubeovnv1.SubnetSpec{Provider: util.OvnProvider},
+	}, {
+		ObjectMeta: metav1.ObjectMeta{Name: "attach-subnet"},
+		Spec:       kubeovnv1.SubnetSpec{Provider: "attachnet-a.default.ovn"},
+	}}
+
+	kubeovnClient := kubeovnfake.NewSimpleClientset()
+	informerFactory := kubeovninformerfactory.NewSharedInformerFactory(kubeovnClient, 0)
+	subnetInformer := informerFactory.Kubeovn().V1().Subnets()
+	for _, subnet := range subnets {
+		require.NoError(t, subnetInformer.Informer().GetStore().Add(subnet))
+	}
+
+	handler := cniServerHandler{Controller: &Controller{subnetsLister: subnetInformer.Lister()}}
+
+	_, ok := handler.providerExists(util.OvnProvider, "")
+	require.True(t, ok)
+
+	subnet, ok := handler.providerExists("attachnet-a.default.ovn", "")
+	require.True(t, ok)
+	require.NotNil(t, subnet)
+	require.Equal(t, "attach-subnet", subnet.Name)
+
+	_, ok = handler.providerExists("attachnet-b.default.ovn", "")
+	require.False(t, ok)
+}
+
+func TestProviderExistsAllowsOnSubnetListError(t *testing.T) {
+	handler := cniServerHandler{Controller: &Controller{subnetsLister: errSubnetLister{}}}
+
+	subnet, ok := handler.providerExists("attachnet-a.default.ovn", "")
+	require.True(t, ok)
+	require.Nil(t, subnet)
 }
