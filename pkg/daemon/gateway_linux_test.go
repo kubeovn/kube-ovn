9c3d07ee745b10ad1838a9f5cd4028380535c4b3
张祖建 <zhangzujian.7@gmail.com>
2026-07-08 10:40:07 +0800

    fix: drop orphan centralized nat outgoing tcp packets (#6998)
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/daemon/gateway_linux_test.go
+++ pkg/daemon/gateway_linux_test.go
@@ -1,12 +1,20 @@
 package daemon
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/kubeovn/kube-ovn/pkg/util"
 )
+
+func TestCentralizedNatOutgoingNonSynDropRule(t *testing.T) {
+	rule := centralizedNatOutgoingNonSynDropRule("10.26.0.0/16", "ovn40subnets")
+	require.Equal(t, MANGLE, rule.Table)
+	require.Equal(t, OvnPostrouting, rule.Chain)
+	require.Equal(t, strings.Fields(`-s 10.26.0.0/16 -p tcp -m tcp --tcp-flags SYN NONE -m conntrack --ctstate NEW -m set ! --match-set ovn40subnets dst -j DROP`), rule.Rule)
+}
 
 func TestFindRulePositionsInList(t *testing.T) {
 	jumpRule := util.IPTableRule{
