457fe53a9386c428dcd8afdca717b6e228fa61c1
zhangzujian <zhangzujian.7@gmail.com>
2026-08-07 14:36:58 +0800

    feat: reconcile OVN ACL sampling objects
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/ovs/interface.go
+++ pkg/ovs/interface.go
@@ -8,6 +8,7 @@ import (
 	v1alpha1 "sigs.k8s.io/network-policy-api/apis/v1alpha1"
 	v1alpha2 "sigs.k8s.io/network-policy-api/apis/v1alpha2"
 
+	"github.com/kubeovn/kube-ovn/pkg/aclsampling"
 	kubeovnv1 "github.com/kubeovn/kube-ovn/pkg/apis/kubeovn/v1"
 	"github.com/kubeovn/kube-ovn/pkg/ovsdb/ovnnb"
 	"github.com/kubeovn/kube-ovn/pkg/ovsdb/ovnsb"
@@ -198,6 +199,10 @@ type ACL interface {
 	CleanNoParentKeyAcls() error
 }
 
+type ACLSampling interface {
+	ReconcileACLSampling(config aclsampling.ControllerConfig) error
+}
+
 type AddressSet interface {
 	CreateAddressSet(asName string, externalIDs map[string]string) error
 	AddressSetUpdateAddress(asName string, addresses ...string) error
@@ -265,6 +270,7 @@ type Meter interface {
 
 type NbClient interface {
 	ACL
+	ACLSampling
 	AddressSet
 	BFD
 	DHCPOptions
