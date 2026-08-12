4e1d57034c0d97ed8361995f1dc58ff051d38f6f
zhangzujian <zhangzujian.7@gmail.com>
2026-08-12 16:37:32 +0800

    fix(daemon): make provider VLAN cleanup upgrade-safe
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/daemon/ovs_linux.go
+++ pkg/daemon/ovs_linux.go
@@ -2380,19 +2380,21 @@ func (c *Controller) transferVlanAddrsToInternalPort(srcName, dstName string) er
 	if err != nil {
 		return fmt.Errorf("failed to get routes on source %s: %w", srcName, err)
 	}
+	return transferProviderVlanRoutes(dst, routes, netlink.RouteReplace)
+}
 
-	for _, route := range routes {
+func transferProviderVlanRoutes(dst netlink.Link, routes []netlink.Route, routeReplace func(*netlink.Route) error) error {
+	for _, current := range routes {
+		route := current
 		if route.Gw == nil && route.Dst != nil && route.Dst.IP.IsLinkLocalUnicast() {
 			continue
 		}
 		route.LinkIndex = dst.Attrs().Index
-		if err = netlink.RouteReplace(&route); err != nil {
-			klog.Warningf("failed to add/replace route %s to destination %s: %v", route.String(), dstName, err)
-		} else {
-			klog.Infof("route %q has been added/replaced to link %s", route.String(), dstName)
+		if err := routeReplace(&route); err != nil {
+			return fmt.Errorf("failed to add/replace route %s on destination %s: %w", route.String(), dst.Attrs().Name, err)
 		}
+		klog.Infof("route %q has been added/replaced to link %s", route.String(), dst.Attrs().Name)
 	}
-
 	return nil
 }
 
