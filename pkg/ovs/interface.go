8cde43ecda93316699f6f7f7a27a46ab5550123e
张祖建 <zhangzujian.7@gmail.com>
2026-08-27 13:21:03 +0800

    feat: attach sampling to NetworkPolicy ACLs (#7163)
    * feat: attach sampling to network policy ACLs
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>
    
    * fix: reject invalid ACL priority constants
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>
    
    * fix: isolate NetworkPolicy ACL sampling
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>
    
    * fix: reuse ACL samples across isolated transactions
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>
    
    ---------
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>
    (cherry picked from commit 9e8fefe95af3dcb40c08d78dd07241f3b5f76346)

--- pkg/ovs/interface.go
+++ pkg/ovs/interface.go
@@ -200,6 +200,8 @@ type ACL interface {
 
 type ACLSampling interface {
 	ReconcileACLSampling(config aclsampling.ControllerConfig) error
+	PrepareNetworkPolicyACLSampling(pgName, namespace, name, uid string) (*NetworkPolicySamplingRequest, error)
+	ApplyNetworkPolicyACLSampling(config aclsampling.ControllerConfig, request *NetworkPolicySamplingRequest) error
 }
 
 type AddressSet interface {
