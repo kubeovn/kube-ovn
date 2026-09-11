4eb465a3d3b5c89c7c366895ccca1360b96d86dc
zhangzujian <zhangzujian.7@gmail.com>
2026-09-11 10:40:39 +0000

    chore: use promoted fields in kubevirt live migration helpers
    modernize rejects explicit embedded field types in struct literals.
    
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/controller/pod_test.go
+++ pkg/controller/pod_test.go
@@ -2695,15 +2695,13 @@ func TestNeedAllocateSubnets(t *testing.T) {
 
 	t.Run("terminating migration pod does not reallocate", func(t *testing.T) {
 		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:                       "virt-launcher-vm",
-				Namespace:                  "ns",
-				DeletionTimestamp:          &now,
-				DeletionGracePeriodSeconds: new(int64(30)),
-				Annotations: map[string]string{
-					kubevirtv1.MigrationJobNameAnnotation: "mig-1",
-					allocatedKey:                          "true",
-				},
+			Name:                       "virt-launcher-vm",
+			Namespace:                  "ns",
+			DeletionTimestamp:          &now,
+			DeletionGracePeriodSeconds: new(int64(30)),
+			Annotations: map[string]string{
+				kubevirtv1.MigrationJobNameAnnotation: "mig-1",
+				allocatedKey:                          "true",
 			},
 			Status: corev1.PodStatus{Phase: corev1.PodRunning},
 		}
@@ -2712,13 +2710,11 @@ func TestNeedAllocateSubnets(t *testing.T) {
 
 	t.Run("live migration pod reallocates", func(t *testing.T) {
 		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "virt-launcher-vm",
-				Namespace: "ns",
-				Annotations: map[string]string{
-					kubevirtv1.MigrationJobNameAnnotation: "mig-1",
-					allocatedKey:                          "true",
-				},
+			Name:      "virt-launcher-vm",
+			Namespace: "ns",
+			Annotations: map[string]string{
+				kubevirtv1.MigrationJobNameAnnotation: "mig-1",
+				allocatedKey:                          "true",
 			},
 			Status: corev1.PodStatus{Phase: corev1.PodRunning},
 		}
@@ -2727,12 +2723,10 @@ func TestNeedAllocateSubnets(t *testing.T) {
 
 	t.Run("allocated pod without migration annotation does not reallocate", func(t *testing.T) {
 		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "pod",
-				Namespace: "ns",
-				Annotations: map[string]string{
-					allocatedKey: "true",
-				},
+			Name:      "pod",
+			Namespace: "ns",
+			Annotations: map[string]string{
+				allocatedKey: "true",
 			},
 			Status: corev1.PodStatus{Phase: corev1.PodRunning},
 		}
@@ -2741,11 +2735,9 @@ func TestNeedAllocateSubnets(t *testing.T) {
 
 	t.Run("unallocated live pod allocates", func(t *testing.T) {
 		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "pod",
-				Namespace: "ns",
-			},
-			Status: corev1.PodStatus{Phase: corev1.PodRunning},
+			Name:      "pod",
+			Namespace: "ns",
+			Status:    corev1.PodStatus{Phase: corev1.PodRunning},
 		}
 		require.Equal(t, nets, needAllocateSubnets(pod, nets))
 	})
