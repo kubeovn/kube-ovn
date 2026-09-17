ba9c4f58a63b08dc388122ab1f2e6b78eae44213
zhangzujian <zhangzujian.7@gmail.com>
2026-09-17 14:47:35 +0000

    test: cover initial monitor integration
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- pkg/ovsdb/client/client.go
+++ pkg/ovsdb/client/client.go
@@ -55,11 +55,19 @@ func NamedUUID() string {
 	return fmt.Sprintf("u%010d", atomic.AddUint32(&namedUUIDCounter, 1))
 }
 
+type initialMonitorClient interface {
+	NewMonitor(...client.MonitorOption) *client.Monitor
+	Monitor(context.Context, *client.Monitor) (client.MonitorCookie, error)
+}
+
 // monitorWithTimeout bounds the initial monitor RPC so cache update errors cannot block startup forever.
-func monitorWithTimeout(monitor func(context.Context) error, timeout time.Duration) error {
+func monitorWithTimeout(c initialMonitorClient, monitors []client.MonitorOption, timeout time.Duration) error {
+	monitor := c.NewMonitor(monitors...)
+	monitor.Method = ovsdb.ConditionalMonitorRPC
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
-	return monitor(ctx)
+	_, err := c.Monitor(ctx, monitor)
+	return err
 }
 
 // NewOvsDbClient creates a new ovsdb client
@@ -130,12 +138,7 @@ func NewOvsDbClient(
 
 	if len(monitors) != 0 {
 		klog.Infof("setting up monitors for %s database on server %s", db, addr)
-		monitor := c.NewMonitor(monitors...)
-		monitor.Method = ovsdb.ConditionalMonitorRPC
-		if err = monitorWithTimeout(func(ctx context.Context) error {
-			_, err := c.Monitor(ctx, monitor)
-			return err
-		}, connectTimeout); err != nil {
+		if err = monitorWithTimeout(c, monitors, connectTimeout); err != nil {
 			c.Close()
 			klog.Errorf("failed to monitor database on %s server %s: %v", db, addr, err)
 			return nil, err
--- pkg/ovsdb/client/client_test.go
+++ pkg/ovsdb/client/client_test.go
@@ -6,64 +6,72 @@ import (
 	"testing"
 	"time"
 
+	libovsdbclient "github.com/ovn-kubernetes/libovsdb/client"
+	"github.com/ovn-kubernetes/libovsdb/ovsdb"
 	"github.com/stretchr/testify/require"
 )
 
+type blockingMonitorClient struct {
+	rpcMutex          sync.RWMutex
+	disconnectStarted chan struct{}
+	disconnectDone    chan struct{}
+	releaseMonitor    chan struct{}
+	monitorMethod     string
+}
+
+func newBlockingMonitorClient() *blockingMonitorClient {
+	return &blockingMonitorClient{
+		disconnectStarted: make(chan struct{}),
+		disconnectDone:    make(chan struct{}),
+		releaseMonitor:    make(chan struct{}),
+	}
+}
+
+func (*blockingMonitorClient) NewMonitor(...libovsdbclient.MonitorOption) *libovsdbclient.Monitor {
+	return &libovsdbclient.Monitor{}
+}
+
+func (c *blockingMonitorClient) Monitor(ctx context.Context, monitor *libovsdbclient.Monitor) (libovsdbclient.MonitorCookie, error) {
+	c.monitorMethod = monitor.Method
+	c.rpcMutex.RLock()
+	defer c.rpcMutex.RUnlock()
+
+	go func() {
+		close(c.disconnectStarted)
+		c.rpcMutex.Lock()
+		close(c.disconnectDone)
+		c.rpcMutex.Unlock()
+	}()
+	<-c.disconnectStarted
+	select {
+	case <-ctx.Done():
+		return libovsdbclient.MonitorCookie{}, ctx.Err()
+	case <-c.releaseMonitor:
+		return libovsdbclient.MonitorCookie{}, nil
+	}
+}
+
 func TestMonitorWithTimeoutBreaksReconnectDeadlock(t *testing.T) {
-	runMonitor := func(ctx context.Context, rpcMutex *sync.RWMutex, disconnectDone chan struct{}, release <-chan struct{}) error {
-		rpcMutex.RLock()
-		defer rpcMutex.RUnlock()
+	c := newBlockingMonitorClient()
+	monitorDone := make(chan error, 1)
+	go func() {
+		monitorDone <- monitorWithTimeout(c, nil, 10*time.Millisecond)
+	}()
 
-		go func() {
-			rpcMutex.Lock()
-			close(disconnectDone)
-			rpcMutex.Unlock()
-		}()
-
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-release:
-			return nil
-		}
+	var err error
+	select {
+	case err = <-monitorDone:
+	case <-time.After(time.Second):
+		close(c.releaseMonitor)
+		<-monitorDone
+		t.Fatal("initial monitor did not honor its timeout")
 	}
 
-	legacyDone := make(chan error, 1)
-	legacyRelease := make(chan struct{})
-	legacyDisconnectDone := make(chan struct{})
-	var legacyMutex sync.RWMutex
-	go func() {
-		legacyDone <- runMonitor(context.Background(), &legacyMutex, legacyDisconnectDone, legacyRelease)
-	}()
-	require.Never(t, func() bool {
-		select {
-		case <-legacyDone:
-			return true
-		default:
-			return false
-		}
-	}, 20*time.Millisecond, time.Millisecond)
-	close(legacyRelease)
-	require.NoError(t, <-legacyDone)
-	require.Eventually(t, func() bool {
-		select {
-		case <-legacyDisconnectDone:
-			return true
-		default:
-			return false
-		}
-	}, time.Second, time.Millisecond)
-
-	disconnectDone := make(chan struct{})
-	var rpcMutex sync.RWMutex
-	err := monitorWithTimeout(func(ctx context.Context) error {
-		return runMonitor(ctx, &rpcMutex, disconnectDone, make(chan struct{}))
-	}, 10*time.Millisecond)
-
 	require.ErrorIs(t, err, context.DeadlineExceeded)
+	require.Equal(t, ovsdb.ConditionalMonitorRPC, c.monitorMethod)
 	require.Eventually(t, func() bool {
 		select {
-		case <-disconnectDone:
+		case <-c.disconnectDone:
 			return true
 		default:
 			return false
